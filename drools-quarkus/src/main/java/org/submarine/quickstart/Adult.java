7b3e1cbc36219342dfcdd76b52732ccd91020fd4
mariofusco <mario.fusco@gmail.com>
2019-03-07 11:51:09 +0100

    first commit

---
+++ drools-quarkus/src/main/java/org/submarine/quickstart/Adult.java
@@ -0,0 +1,14 @@
+package org.submarine.quickstart;
+
+public class Adult {
+
+    private final Person person;
+
+    public Adult(Person p) {
+        person = p;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+}
