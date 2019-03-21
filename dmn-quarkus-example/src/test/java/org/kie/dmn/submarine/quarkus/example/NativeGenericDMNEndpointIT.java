3f8b9db77bbb3899b23f4b9429dc0e63769a257e
tarilabs <matteo.mortari@gmail.com>
2019-03-21 18:59:07 +0100

    DROOLS-3814 DMN Submarine Example on Quarkus

---
+++ dmn-quarkus-example/src/test/java/org/kie/dmn/submarine/quarkus/example/NativeGenericDMNEndpointIT.java
@@ -0,0 +1,9 @@
+package org.kie.dmn.submarine.quarkus.example;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeGenericDMNEndpointIT extends GenericDMNEndpointTest {
+
+    // Execute the same tests but in native mode.
+}
