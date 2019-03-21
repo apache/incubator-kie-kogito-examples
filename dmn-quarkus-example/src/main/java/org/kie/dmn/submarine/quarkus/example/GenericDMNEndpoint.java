c58ab69de962868dcc89b25b1f03f63fa1a1ad99
tarilabs <matteo.mortari@gmail.com>
2019-03-21 18:59:07 +0100

    DROOLS-3814 DMN Submarine Example on Quarkus

---
+++ dmn-quarkus-example/src/main/java/org/kie/dmn/submarine/quarkus/example/GenericDMNEndpoint.java
@@ -0,0 +1,68 @@
+package org.kie.dmn.submarine.quarkus.example;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNRuntime;
+import org.kie.dmn.submarine.rest.quarkus.DMNModelInfo;
+import org.kie.dmn.submarine.rest.quarkus.DMNModelInfoList;
+import org.kie.dmn.submarine.rest.quarkus.DMNResult;
+import org.kie.dmn.submarine.rest.quarkus.DMNSubmarineQuarkus;
+
+@Path("/dmn")
+public class GenericDMNEndpoint {
+
+    static final DMNRuntime dmnRuntime = DMNSubmarineQuarkus.createGenericDMNRuntime();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public DMNResult dmn(@HeaderParam("X-DMN-model-namespace") String modelNamespace,
+                         @HeaderParam("X-DMN-model-name") String modelName,
+                         Map<String, Object> dmnContext) {
+        if (dmnRuntime.getModels().size() > 1 && (modelNamespace == null || modelName == null)) {
+            throw new MultipleModelsMissingParams();
+        }
+        return DMNSubmarineQuarkus.evaluate(dmnRuntime,
+                                            modelNamespace,
+                                            modelName,
+                                            dmnContext);
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public DMNModelInfoList dmn() {
+        List<DMNModel> models = dmnRuntime.getModels();
+        List<DMNModelInfo> result = models.stream().map(DMNModelInfo::of).collect(Collectors.toList());
+        return new DMNModelInfoList(result);
+    }
+
+    public static class MultipleModelsMissingParams extends RuntimeException {
+
+        public MultipleModelsMissingParams() {
+            super("There are multiple DMN models. MUST provide X-DMN-model-namespace, X-DMN-model-name header parameters, to indentify DMN model to be evaluated.");
+        }
+    }
+
+    @Provider
+    public static class MultipleModelsMissingParamsMapper implements ExceptionMapper<MultipleModelsMissingParams> {
+
+        public Response toResponse(MultipleModelsMissingParams e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+}
