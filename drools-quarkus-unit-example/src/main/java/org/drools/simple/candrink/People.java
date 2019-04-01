325069ed89cf2871739f84d71adc0c180551fd1a
Edoardo Vacchi <uncommonnonsense@gmail.com>
2019-04-01 14:03:45 +0200

    DROOLS-3763: New API codegen prototype

---
+++ drools-quarkus-unit-example/src/main/java/org/drools/simple/candrink/People.java
@@ -0,0 +1,21 @@
+package org.drools.simple.candrink;
+
+import org.kie.submarine.rules.DataSource;
+import org.kie.submarine.rules.Unit;
+import org.kie.submarine.rules.impl.ListDataSource;
+import org.submarine.quickstart.Person;
+import org.submarine.quickstart.Result;
+
+@Unit
+public class People {
+    private final DataSource<Result> results = new ListDataSource<>();
+    private final DataSource<Person> persons = new ListDataSource<>();
+
+    public DataSource<Result> results() {
+        return results;
+    }
+
+    public DataSource<Person> persons() {
+        return persons;
+    }
+}
