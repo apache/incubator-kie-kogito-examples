4fc575ab9fa4685fe21dd3f0cf8e8b40d4c68bc4
Edoardo Vacchi <uncommonnonsense@gmail.com>
2019-04-01 13:52:49 +0200

    DROOLS-3786: Drools Polyglot Example

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
   - `git clone https://github.com/kiegroup/submarine-bom; cd submarine-bom; mvn install`
   - `git clone https://github.com/kiegroup/submarine-runtimes; cd submarine-runtimes; mvn install`
   
-- Drools on Quarkus: see [README.md](drools-quarkus/README.md)
+- Polyglot Drools with GraalVM: see [README.md](drools-polyglot-example/README.md)
+- Drools on Quarkus: see [README.md](drools-quarkus-example/README.md)
 - jBPM on Quarkus: see [README.md](jbpm-quarkus-example/README.md)
 - jBPM on SpringBoot: see [README.md](jbpm-springboot-example/README.md)
 
---
+++ drools-polyglot-example/src/main/js/drools.mjs
@@ -0,0 +1,24 @@
+const ModelImpl = Java.type('org.drools.model.impl.ModelImpl');
+const DSL = Java.type('org.drools.model.DSL');
+const declarationOf = DSL.declarationOf;
+const PatternDSL = Java.type('org.drools.model.PatternDSL');
+const Rule = PatternDSL.rule;
+const Pattern = PatternDSL.pattern;
+const on = (v1, v2) => new (Java.type('org.submarine.examples.temp.jsinterop.Consequence_2'))(v1,v2);
+const KieBaseBuilder = Java.type('org.drools.modelcompiler.builder.KieBaseBuilder');
+
+function Session(model) {
+    return KieBaseBuilder.createKieBaseFromModel( model ).newKieSession()
+}
+
+function Model(...ms) {
+    const model = new ModelImpl();
+    for (const m of ms) {
+        model.addRule(m)
+    }
+    return model
+}
+
+export {
+    Model, Session, Rule, Pattern, on, declarationOf
+}
