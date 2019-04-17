7181c2afa252bc17dd8c1aad05ca481cab16802d
tarilabs <matteo.mortari@gmail.com>
2019-04-17 09:51:47 +0200

    Add byName endpoint

---
+++ dmn-quarkus-example/src/test/java/org/kie/dmn/submarine/quarkus/example/NativeByNameDMNEndpointIT.java
@@ -0,0 +1,9 @@
+package org.kie.dmn.submarine.quarkus.example;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeByNameDMNEndpointIT extends ByNameDMNEndpointTest {
+
+    // Execute the same tests but in native mode.
+}
