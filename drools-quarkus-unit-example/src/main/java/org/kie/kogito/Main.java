ea8cf7da664ae294e540c676793f8b0da3aaf30e
Edoardo Vacchi <uncommonnonsense@gmail.com>
2019-06-26 16:37:49 +0200

    wip

--- drools-quarkus-unit-example/src/main/java/org/kie/kogito/Main.java
+++ drools-quarkus-unit-example/src/main/java/org/kie/kogito/Main.java
@@ -15,11 +15,16 @@
 
 package org.kie.kogito;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.kie.kogito.rules.alerting.Event;
 import org.kie.kogito.rules.alerting.LoggerService;
 import org.kie.kogito.rules.alerting.LoggerServiceRuleUnit;
+import org.kie.kogito.rules.alerting.LoggerServiceRuleUnitInstance;
 import org.kie.kogito.rules.alerting.MonitoringService;
 import org.kie.kogito.rules.alerting.MonitoringServiceRuleUnit;
+import org.kie.kogito.rules.alerting.MonitoringServiceRuleUnitInstance;
 
 public class Main {
 
@@ -31,13 +36,26 @@ public class Main {
         LoggerService loggerService = new LoggerService(monitoringService.getAlertStream());
 
         Executor executor = Executor.create();
-
-        executor.submit(new MonitoringServiceRuleUnit().createInstance(monitoringService));
-//        executor.submit(new LoggerServiceRuleUnit().createInstance(loggerService));
-
         monitoringService.getEventStream().add(new Event("Hello!"));
         monitoringService.getEventStream().add(new Event("Hello Again!"));
         monitoringService.getEventStream().add(new Event("Hello 3!"));
+
+        MonitoringServiceRuleUnitInstance monitoringServiceInstance = new MonitoringServiceRuleUnit().createInstance(monitoringService);
+//        executor.submit(instance);
+//        executor.submit(new LoggerServiceRuleUnit().createInstance(loggerService));
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        LoggerServiceRuleUnitInstance loggerServiceInstance = new LoggerServiceRuleUnit().createInstance(loggerService);
+//        executorService.submit(monitoringServiceInstance::fire);
+//        executorService.submit(loggerServiceInstance::fire);
+
+//        instance.fire();
+        monitoringService.getEventStream().add(new Event("Hello 4!"));
+//        instance.fire();
+
+        monitoringServiceInstance.fire();
+        loggerServiceInstance.fire();
 
     }
 }
