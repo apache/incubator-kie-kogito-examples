638d896aba11365194e5fcdb5c985395da28f686
Mario Fusco <mario.fusco@gmail.com>
2019-07-03 10:03:46 +0200

    Drools 4241a (#41)
    * use entry points
    
    * wip
    
    * wip
    
    * wip
    
    * [DROOLS-4241] upgrade rule unit example to new API

---
+++ drools-quarkus-unit-example/src/main/java/org/kie/kogito/rules/alerting/LoggerService.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.rules.alerting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kie.kogito.rules.DataStream;
+import org.kie.kogito.rules.RuleUnitMemory;
+
+public class LoggerService implements RuleUnitMemory {
+
+    private final DataStream<Alert> alertStream;
+
+    private final List<String> log = new ArrayList<>();
+
+    public LoggerService( DataStream<Alert> alertStream) {
+        this.alertStream = alertStream;
+    }
+
+    public DataStream<Alert> getAlertStream() {
+        return alertStream;
+    }
+
+    public List<String> getLog() {
+        return log;
+    }
+}
