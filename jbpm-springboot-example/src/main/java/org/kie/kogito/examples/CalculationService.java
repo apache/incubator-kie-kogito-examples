9bc187363e20280e05d8e2a849ec09340439aabe
Maciej Swiderski <swiderski.maciej@gmail.com>
2019-07-04 15:21:37 +0200

    KOGITO-42 - Use Spring specific dependencies for injection in generated code

--- jbpm-springboot-example/src/main/java/org/kie/kogito/examples/CalculationService.java
+++ jbpm-springboot-example/src/main/java/org/kie/kogito/examples/CalculationService.java
@@ -3,7 +3,9 @@ package org.kie.kogito.examples;
 import java.util.Random;
 
 import org.kie.kogito.examples.demo.Order;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CalculationService {
 
     private Random random = new Random();
--- jbpm-springboot-example/src/main/java/org/kie/kogito/examples/DemoApplication.java
+++ jbpm-springboot-example/src/main/java/org/kie/kogito/examples/DemoApplication.java
@@ -3,7 +3,7 @@ package org.kie.kogito.examples;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"org.kie.kogito.**"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
