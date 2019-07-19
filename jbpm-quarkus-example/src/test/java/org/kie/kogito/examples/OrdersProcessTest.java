66599f6ebaceafc73c12867bf127028509a99b87
Tihomir Surdilovic <tsurdilo@redhat.com>
2019-07-18 20:32:49 -0400

    adding rest client test for jbpm-quarkus-example (#50)

--- jbpm-quarkus-example/src/test/java/org/kie/kogito/examples/OrdersProcessTest.java
+++ jbpm-quarkus-example/src/test/java/org/kie/kogito/examples/OrdersProcessTest.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kie.kogito.Model;
 import org.kie.kogito.examples.demo.Order;
@@ -21,49 +22,58 @@ import org.kie.kogito.process.WorkItem;
 
 import io.quarkus.test.junit.QuarkusTest;
 
-
 @QuarkusTest
 public class OrdersProcessTest {
 
     @Inject
     @Named("demo.orders")
     Process<? extends Model> orderProcess;
-    
+
     @Inject
     @Named("demo.orderItems")
     Process<? extends Model> orderItemsProcess;
-    
+
+    @BeforeEach
+    public void setup() {
+        // abort all intsances after each test
+        // as other tests might have added instances
+        // needed until Quarkust implements @DirtiesContext similar to springboot
+        // see https://github.com/quarkusio/quarkus/pull/2866
+        orderProcess.instances().values().forEach(pi -> pi.abort());
+        orderItemsProcess.instances().values().forEach(pi -> pi.abort());
+    }
+
     @Test
     public void testOrderProcess() {
         assertNotNull(orderProcess);
-        
+
         Model m = orderProcess.createModel();
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("approver", "john");
         parameters.put("order", new Order("12345", false, 0.0));
         m.fromMap(parameters);
-        
+
         ProcessInstance<?> processInstance = orderProcess.createInstance(m);
         processInstance.start();
-        
+
         assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_ACTIVE, processInstance.status());
-        Model result = (Model)processInstance.variables();
+        Model result = (Model) processInstance.variables();
         assertEquals(2, result.toMap().size());
-        assertTrue(((Order)result.toMap().get("order")).getTotal() > 0);
+        assertTrue(((Order) result.toMap().get("order")).getTotal() > 0);
 
         ProcessInstances<? extends Model> orderItemProcesses = orderItemsProcess.instances();
         assertEquals(1, orderItemProcesses.values().size());
-        
+
         ProcessInstance<?> childProcessInstance = orderItemProcesses.values().iterator().next();
-        
+
         List<WorkItem> workItems = childProcessInstance.workItems();
         assertEquals(1, workItems.size());
-        
+
         childProcessInstance.completeWorkItem(workItems.get(0).getId(), null);
-        
-        assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED, childProcessInstance.status());                        
-        assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED, processInstance.status());        
-        
+
+        assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED, childProcessInstance.status());
+        assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED, processInstance.status());
+
         // no active process instances for both orders and order items processes
         assertEquals(0, orderProcess.instances().values().size());
         assertEquals(0, orderItemsProcess.instances().values().size());
---
+++ jbpm-quarkus-example/src/test/java/org/kie/kogito/examples/OrdersRestTest.java
@@ -0,0 +1,70 @@
+package org.kie.kogito.examples;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kie.kogito.Model;
+import org.kie.kogito.process.Process;
+import org.kie.kogito.process.ProcessInstances;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@QuarkusTest
+public class OrdersRestTest {
+    @Inject
+    @Named("demo.orders")
+    Process<? extends Model> orderProcess;
+
+    @BeforeEach
+    public void setup() {
+        // abort up all intsances after each test
+        // as other tests might have added instances
+        // needed until Quarkust implements @DirtiesContext similar to springboot
+        // see https://github.com/quarkusio/quarkus/pull/2866
+        orderProcess.instances().values().forEach(pi -> pi.abort());
+    }
+
+    @Test
+    public void testOrdersRest() {
+        assertNotNull(orderProcess);
+
+        // test adding new order
+        String addOrderPayload = "{\"approver\" : \"john\", \"order\" : {\"orderNumber\" : \"12345\", \"shipped\" : false}}";
+        int firstCreatedId = given().contentType(ContentType.JSON).accept(ContentType.JSON).body(addOrderPayload).when()
+                .post("/orders").then().statusCode(200).body("id", notNullValue()).extract().path("id");
+
+        // test getting the created order
+        given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders").then().statusCode(200)
+                .body("$.size()", is(1), "[0].id", is(firstCreatedId));
+
+        // test getting order by id
+        given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders/" + firstCreatedId).then()
+                .statusCode(200).body("id", is(firstCreatedId));
+
+        // test delete order
+        // first add second order...
+        int secondCreatedId = given().contentType(ContentType.JSON).accept(ContentType.JSON).body(addOrderPayload)
+                .when().post("/orders").then().statusCode(200).body("id", notNullValue()).extract().path("id");
+        // now delete the first order created
+        given().accept(ContentType.JSON).when().delete("/orders/" + firstCreatedId).then().statusCode(200);
+        // get all orders make sure there is only one
+        given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders").then().statusCode(200)
+                .body("$.size()", is(1), "[0].id", is(secondCreatedId));
+
+        // delete second before finishing
+        given().accept(ContentType.JSON).when().delete("/orders/" + secondCreatedId).then().statusCode(200);
+        // get all orders make sure there is zero
+        given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders").then().statusCode(200)
+                .body("$.size()", is(0));
+    }
+
+}
