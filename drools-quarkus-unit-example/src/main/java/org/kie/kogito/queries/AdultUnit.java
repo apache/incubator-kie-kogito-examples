872a76a50f213622b559d60c7c9101c2a6c8f69e
Mario Fusco <mario.fusco@gmail.com>
2019-07-29 11:02:21 +0200

    [KOGITO-56] first implementation of automatic rest endpoint generatio… (#47)
    * [KOGITO-56] first implementation of automatic rest endpoint generation for queries
    
    * remove unused imports
    
    * [KOGITO-56] first implementation of automatic rest endpoint generation for queries
    
    * [KOGITO-56] first implementation of automatic rest endpoint generation for queries
    
    * [KOGITO-56] first implementation of automatic rest endpoint generation for queries

---
+++ drools-quarkus-unit-example/src/main/java/org/kie/kogito/queries/AdultUnit.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2005 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.queries;
+
+import java.io.Serializable;
+
+import org.kie.kogito.rules.DataSource;
+import org.kie.kogito.rules.DataStore;
+import org.kie.kogito.rules.RuleUnitMemory;
+
+public class AdultUnit implements RuleUnitMemory, Serializable {
+    private int adultAge;
+
+    private DataStore<Person> persons;
+
+    public AdultUnit( ) {
+        this( DataSource.createStore() );
+    }
+
+    public AdultUnit( DataStore<Person> persons ) {
+        this.persons = persons;
+    }
+
+    public DataStore<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons( DataStore<Person> persons ) {
+        this.persons = persons;
+    }
+
+    public int getAdultAge() {
+        return adultAge;
+    }
+
+    public void setAdultAge( int adultAge ) {
+        this.adultAge = adultAge;
+    }
+}
---
+++ drools-quarkus-unit-example/src/test/java/org/kie/kogito/QueryTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.kie.kogito.queries.AdultUnit;
+import org.kie.kogito.queries.AdultUnitQueryFindAdultsEndpoint;
+import org.kie.kogito.queries.AdultUnitQueryFindAdultNamesEndpoint;
+import org.kie.kogito.queries.AdultUnitQueryFindNotAdultNamesAndAgeEndpoint;
+import org.kie.kogito.queries.AdultUnitRuleUnit;
+import org.kie.kogito.queries.Person;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QueryTest {
+
+    @Test
+    public void testPersons() {
+        AdultUnitQueryFindAdultsEndpoint query = new AdultUnitQueryFindAdultsEndpoint(new AdultUnitRuleUnit());
+
+        List<String> results = query.executeQuery( createAdultUnit() )
+                .stream()
+                .map( Person::getName )
+                .collect( toList() );
+
+        assertEquals( 2, results.size() );
+        assertTrue( results.containsAll( asList("Mario", "Marilena") ) );
+    }
+
+    @Test
+    public void testNames() {
+        AdultUnitQueryFindAdultNamesEndpoint query = new AdultUnitQueryFindAdultNamesEndpoint(new AdultUnitRuleUnit());
+
+        List<String> results = query.executeQuery( createAdultUnit() );
+
+        assertEquals( 2, results.size() );
+        assertTrue( results.containsAll( asList("Mario", "Marilena") ) );
+    }
+
+    @Test
+    public void testResult() {
+        AdultUnitQueryFindNotAdultNamesAndAgeEndpoint query = new AdultUnitQueryFindNotAdultNamesAndAgeEndpoint(new AdultUnitRuleUnit());
+
+        List<AdultUnitQueryFindNotAdultNamesAndAgeEndpoint.Result> results = query.executeQuery( createAdultUnit() );
+
+        assertEquals( 1, results.size() );
+        AdultUnitQueryFindNotAdultNamesAndAgeEndpoint.Result result = results.get(0);
+        assertEquals( "Sofia", result.get$name() );
+        assertEquals( 7, result.get$age() );
+    }
+
+    private AdultUnit createAdultUnit() {
+        org.kie.kogito.examples.Application application = new org.kie.kogito.examples.Application();
+
+        AdultUnit adults = new AdultUnit();
+
+        adults.getPersons().add( new Person( "Mario", 45 ) );
+        adults.getPersons().add( new Person( "Marilena", 47 ) );
+        adults.getPersons().add( new Person( "Sofia", 7 ) );
+        return adults;
+    }
+}
