01916cf03ca61f90cce5348ecaaa2e2bfe39e91b
Maciej Swiderski <swiderski.maciej@gmail.com>
2019-08-07 10:27:26 +0200

    KOGITO-118 - Separate Application from ApplicationConfig in Kogito codegen

--- onboarding-example/hr/src/main/java/org/kie/kogito/examples/RuleEventListenerConfig.java
+++ onboarding-example/hr/src/main/java/org/kie/kogito/examples/RuleEventListenerConfig.java
@@ -1,9 +1,11 @@
 package org.kie.kogito.examples;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.drools.core.config.DefaultRuleEventListenerConfig;
 import org.kie.addons.monitoring.rule.PrometheusMetricsDroolsListener;
 
-
+@ApplicationScoped
 public class RuleEventListenerConfig extends DefaultRuleEventListenerConfig {
 
     
--- onboarding-example/onboarding/src/main/java/org/kie/kogito/examples/ProcessEventListenerConfig.java
+++ onboarding-example/onboarding/src/main/java/org/kie/kogito/examples/ProcessEventListenerConfig.java
@@ -1,9 +1,11 @@
 package org.kie.kogito.examples;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.kie.addons.monitoring.process.PrometheusProcessEventListener;
 import org.kie.kogito.process.impl.DefaultProcessEventListenerConfig;
 
-
+@ApplicationScoped
 public class ProcessEventListenerConfig extends DefaultProcessEventListenerConfig {
    
     public ProcessEventListenerConfig() {
