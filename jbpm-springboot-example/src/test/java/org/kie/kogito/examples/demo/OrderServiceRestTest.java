1f1323690d927ba8c4aee5024642c4e232ec0cfd
Maciej Swiderski <swiderski.maciej@gmail.com>
2019-09-06 15:16:32 +0200

    KOGITO-250 - Generate REST endpoint for private processes but without option to create new instances, KOGITO-251 - Expose service url as part of the kogito events

--- jbpm-springboot-example/src/test/java/org/kie/kogito/examples/demo/OrderServiceRestTest.java
+++ jbpm-springboot-example/src/test/java/org/kie/kogito/examples/demo/OrderServiceRestTest.java
@@ -3,7 +3,10 @@ package org.kie.kogito.examples.demo;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -139,6 +142,94 @@ public class OrderServiceRestTest {
         given().accept(ContentType.JSON).when().delete("/orders/" + firstCreatedId).then().statusCode(200);
         // get all orders make sure there is zero
         given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders").then().statusCode(200)
+                .body("$.size()", is(0));
+    }
+    
+    @Test
+    public void testOrdersWithOrderItemsRest() {
+
+        // test adding new order
+        String addOrderPayload = "{\"approver\" : \"john\", \"order\" : {\"orderNumber\" : \"12345\", \"shipped\" : false}}";
+        String firstCreatedId = given().contentType(ContentType.JSON).accept(ContentType.JSON).body(addOrderPayload).when()
+                .post("/orders").then().statusCode(200).body("id", notNullValue()).extract().path("id");
+
+        // test getting the created order
+        given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders").then().statusCode(200)
+                .body("$.size()", is(1), "[0].id", is(firstCreatedId));
+
+        // test getting order by id
+        given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders/" + firstCreatedId).then()
+                .statusCode(200).body("id", is(firstCreatedId));
+
+        // test getting order items subprocess
+        String orderItemsId = given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orderItems").then().statusCode(200)
+                .body("$.size()", is(1)).extract().path("[0].id");
+        
+        // test getting order items by id
+        given().accept(ContentType.JSON).when().get("/orderItems/" + orderItemsId).then()
+                .statusCode(200).body("id", is(orderItemsId));
+        
+        // test getting task
+        Map taskInfo = given().accept(ContentType.JSON).when().get("/orderItems/" + orderItemsId + "/tasks").then()
+        .statusCode(200).extract().as(Map.class);
+        
+        assertEquals(1, taskInfo.size());
+        taskInfo.containsValue("Verify_order");
+        
+        // test completing task
+        String payload = "{}";
+        given().contentType(ContentType.JSON).accept(ContentType.JSON).body(payload).when().post("/orderItems/" + orderItemsId + "/Verify_order/" + taskInfo.keySet().iterator().next()).then()
+        .statusCode(200).body("id", is(orderItemsId));
+        
+        // get all orders make sure there is zero
+        given().accept(ContentType.JSON).when().get("/orders").then().statusCode(200)
+                .body("$.size()", is(0));
+        
+        // get all order items make sure there is zero
+        given().accept(ContentType.JSON).when().get("/orderItems").then().statusCode(200)
+                .body("$.size()", is(0));
+    }
+    
+    @Test
+    public void testOrdersWithOrderItemsAbortedRest() {
+
+        // test adding new order
+        String addOrderPayload = "{\"approver\" : \"john\", \"order\" : {\"orderNumber\" : \"12345\", \"shipped\" : false}}";
+        String firstCreatedId = given().contentType(ContentType.JSON).accept(ContentType.JSON).body(addOrderPayload).when()
+                .post("/orders").then().statusCode(200).body("id", notNullValue()).extract().path("id");
+
+        // test getting the created order
+        given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders").then().statusCode(200)
+                .body("$.size()", is(1), "[0].id", is(firstCreatedId));
+
+        // test getting order by id
+        given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orders/" + firstCreatedId).then()
+                .statusCode(200).body("id", is(firstCreatedId));
+
+        // test getting order items subprocess
+        String orderItemsId = given().accept(ContentType.JSON).body(addOrderPayload).when().get("/orderItems").then().statusCode(200)
+                .body("$.size()", is(1)).extract().path("[0].id");
+        
+        // test getting order items by id
+        given().accept(ContentType.JSON).when().get("/orderItems/" + orderItemsId).then()
+                .statusCode(200).body("id", is(orderItemsId));
+        
+        // test getting task
+        Map taskInfo = given().accept(ContentType.JSON).when().get("/orderItems/" + orderItemsId + "/tasks").then()
+        .statusCode(200).extract().as(Map.class);
+        
+        assertEquals(1, taskInfo.size());
+        taskInfo.containsValue("Verify_order");
+        
+        // test deleting order items
+        given().accept(ContentType.JSON).when().delete("/orderItems/" + orderItemsId).then().statusCode(200);
+        
+        // get all orders make sure there is zero
+        given().accept(ContentType.JSON).when().get("/orders").then().statusCode(200)
+                .body("$.size()", is(0));
+        
+        // get all order items make sure there is zero
+        given().accept(ContentType.JSON).when().get("/orderItems").then().statusCode(200)
                 .body("$.size()", is(0));
     }
 }
