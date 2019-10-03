55f2af28d7f073291c85b6f7810773b40c386427
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2019-10-03 14:33:49 +1000

    Add config for data index url (#10)

---
+++ 04-kogito-visas/src/main/java/org/kie/kogito/app/VertxRouter.java
@@ -0,0 +1,48 @@
+package org.kie.kogito.app;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import io.quarkus.vertx.web.Route;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.ext.web.RoutingContext;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import static io.vertx.core.http.HttpMethod.GET;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@ApplicationScoped
+public class VertxRouter {
+
+    @Inject
+    @ConfigProperty(name = "kogito.dataindex.url", defaultValue = "http://localhost:8180/graphql")
+    private String dataIndexURL;
+
+    @Inject
+    private Vertx vertx;
+
+    private String resource;
+
+    @PostConstruct
+    public void init() {
+        resource = vertx.fileSystem()
+                .readFileBlocking("META-INF/resources/index.html")
+                .toString(UTF_8)
+                .replace("__GRAPHIQL_ENDPOINT__", "\"" + dataIndexURL + "\"");
+    }
+
+    @Route(path = "/", methods = GET)
+    public void handle(RoutingContext context) {
+        try {
+            context.response()
+                    .putHeader(HttpHeaders.CACHE_CONTROL, "no-cache")
+                    .putHeader(HttpHeaders.CONTENT_TYPE, "text/html;charset=utf8")
+                    .end(resource);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            context.fail(500, ex);
+        }
+    }
+}
