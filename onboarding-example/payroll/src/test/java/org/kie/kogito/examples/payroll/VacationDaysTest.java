282f653f242b1e4bddf6c0fd201f6ba05e217300
tarilabs <matteo.mortari@gmail.com>
2019-10-09 17:49:42 +0200

    Align vacationDays.dmn

--- onboarding-example/payroll/src/test/java/org/kie/kogito/examples/payroll/VacationDaysTest.java
+++ onboarding-example/payroll/src/test/java/org/kie/kogito/examples/payroll/VacationDaysTest.java
@@ -16,13 +16,12 @@
 
 package org.kie.kogito.examples.payroll;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-
-import org.junit.jupiter.api.Test;
-
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 public class VacationDaysTest {
@@ -42,7 +41,7 @@ public class VacationDaysTest {
                .body("{\"employee\" : {\"firstName\" : \"Mark\", \"lastName\" : \"Test\", \"personalId\" : \"xxx-yy-zzz\", \"birthDate\" : \"1995-12-10T14:50:12.123+02:00\", \"address\" : {\"country\" : \""+country+"\", \"city\" : \"Boston\", \"street\" : \"any street 3\", \"zipCode\" : \"10001\"}}}")
                .contentType(ContentType.JSON)
           .when()
-               .post("/vacationDays")
+               .post("/vacationDays/payrollService")
           .then()
              .statusCode(200)
                .body("vacationDays", is(result));
