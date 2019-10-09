596c273e85075da8a4a3dd066064b9624135e784
tarilabs <matteo.mortari@gmail.com>
2019-10-09 17:52:13 +0200

    Align dmn-quarkus-example test

--- dmn-quarkus-example/src/test/java/org/kie/dmn/kogito/quarkus/example/TrafficViolationTest.java
+++ dmn-quarkus-example/src/test/java/org/kie/dmn/kogito/quarkus/example/TrafficViolationTest.java
@@ -21,9 +21,6 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
@@ -47,8 +44,6 @@ public class TrafficViolationTest {
                .post("/Traffic Violation")
           .then()
              .statusCode(200)
-               .body("'dmn-context'.'Should the driver be suspended?'", is("No"))
-               .body("decision-results", hasItem(allOf(hasEntry("decision-name", "Should the driver be suspended?"),
-                                                       hasEntry("result", "No"))));
+               .body("'Should the driver be suspended?'", is("No"));
     }
 }
