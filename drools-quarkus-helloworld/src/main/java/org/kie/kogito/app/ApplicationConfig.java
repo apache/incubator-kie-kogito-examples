a6f67dab9365bbc05ae20390fff13c6b0de9f7ef
Edoardo Vacchi <uncommonnonsense@gmail.com>
2019-12-18 13:53:51 +0100

    fix app config

--- drools-quarkus-helloworld/src/main/java/org/kie/kogito/app/ApplicationConfig.java
+++ drools-quarkus-helloworld/src/main/java/org/kie/kogito/app/ApplicationConfig.java
@@ -3,16 +3,9 @@ package org.kie.kogito.app;
 import javax.inject.Singleton;
 
 import org.drools.core.config.DefaultRuleEventListenerConfig;
-import org.kie.api.conf.EventProcessingOption;
-import org.kie.api.runtime.conf.ClockTypeOption;
 import org.kie.kogito.process.ProcessConfig;
-import org.kie.kogito.process.ProcessEventListenerConfig;
-import org.kie.kogito.process.WorkItemHandlerConfig;
-import org.kie.kogito.process.impl.DefaultWorkItemHandlerConfig;
 import org.kie.kogito.rules.RuleConfig;
 import org.kie.kogito.rules.RuleEventListenerConfig;
-import org.kie.kogito.signal.SignalManagerHub;
-import org.kie.kogito.uow.UnitOfWorkManager;
 
 /**
  * This is an example custom configuration. You can delete this file entirely
@@ -35,13 +28,13 @@ public class ApplicationConfig  implements org.kie.kogito.Config {
             }
 
             @Override
-            public EventProcessingOption eventProcessingMode() {
-                return EventProcessingOption.CLOUD;
+            public EventProcessing eventProcessingMode() {
+                return EventProcessing.Cloud;
             }
 
             @Override
-            public ClockTypeOption clockType() {
-                return ClockTypeOption.REALTIME;
+            public ClockType clockType() {
+                return ClockType.RealTime;
             }
         };
     }
