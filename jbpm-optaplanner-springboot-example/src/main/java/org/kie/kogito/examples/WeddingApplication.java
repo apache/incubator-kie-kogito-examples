2c9f485be6e4792aa0cbac6ac332231e4a042754
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-01-09 09:59:20 +0100

    Add frontend static html and js

--- jbpm-optaplanner-springboot-example/src/main/java/org/kie/kogito/examples/WeddingApplication.java
+++ jbpm-optaplanner-springboot-example/src/main/java/org/kie/kogito/examples/WeddingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={"org.kie.kogito.**"})
-public class DemoApplication {
+public class WeddingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WeddingApplication.class, args);
 	}
 
 }
