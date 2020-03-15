2c2454bd48fadbe21a7c048528f370fdeda588cc
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2020-03-16 00:51:19 +1000

    KOGITO-1448 - Upgrade Quarkus to version 1.3.0
    - review async timer tests

--- kogito-timer-quarkus/src/test/java/org/acme/travels/CycleTimersProcessTest.java
+++ kogito-timer-quarkus/src/test/java/org/acme/travels/CycleTimersProcessTest.java
@@ -1,49 +1,50 @@
 package org.acme.travels;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.kie.kogito.Application;
+import org.kie.kogito.Model;
+import org.kie.kogito.process.Process;
+import org.kie.kogito.process.ProcessInstance;
+import org.kie.kogito.process.impl.DefaultProcessEventListenerConfig;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import org.junit.jupiter.api.Test;
-import org.kie.kogito.Model;
-import org.kie.kogito.process.Process;
-import org.kie.kogito.process.ProcessInstance;
-
-import io.quarkus.test.junit.QuarkusTest;
-
 @QuarkusTest
 public class CycleTimersProcessTest {
 
-    
-    @Named("timerscycle")
     @Inject
-    Process<? extends Model> timersProcess;
-    
+    Application app;
+
     @Test
     public void testTimersCycleProcess() throws InterruptedException {
-                
+        CountDownLatch latch = new CountDownLatch(1);
+        ((DefaultProcessEventListenerConfig) app.config().process().processEventListeners()).register(new CompleteProcessListener(latch));
+
+        Process<? extends Model> timersProcess = app.processes().processById("timerscycle");
         assertNotNull(timersProcess);
-        
         Model m = timersProcess.createModel();
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("delay", "R2/PT2S");
         m.fromMap(parameters);
-        
+
         ProcessInstance<?> processInstance = timersProcess.createInstance(m);
         processInstance.start();
-        assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_ACTIVE, processInstance.status()); 
-        
-        Thread.sleep(5000);
-        
+        assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_ACTIVE, processInstance.status());
+
+        latch.await(1, TimeUnit.MINUTES);
+
         Optional<? extends ProcessInstance<?>> exists = timersProcess.instances().findById(processInstance.id());
         assertFalse(exists.isPresent());
-       
     }
 }
