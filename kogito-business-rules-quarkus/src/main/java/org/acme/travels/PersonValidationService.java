b7cda3c5aebda4abf9032f0fedbf8c848f503ca4
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-03-20 09:42:37 +0100

    KOGITO-1029: Convert quickstarts to rule units

---
+++ kogito-business-rules-quarkus/src/main/java/org/acme/travels/PersonValidationService.java
@@ -0,0 +1,14 @@
+package org.acme.travels;
+
+import org.kie.kogito.examples.demo.Person;
+import org.kie.kogito.rules.DataSource;
+import org.kie.kogito.rules.DataStore;
+import org.kie.kogito.rules.RuleUnitData;
+
+public class PersonValidationService implements RuleUnitData {
+    private DataStore<Person> persons = DataSource.createStore();
+
+    public DataStore<Person> getPersons() {
+        return persons;
+    }
+}
