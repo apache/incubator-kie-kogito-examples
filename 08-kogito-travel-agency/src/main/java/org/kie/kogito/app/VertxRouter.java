e515cf4fb1ee39217597e895c327167f13c4f89f
Duncan Doyle <Duncan.Doyle@gmail.com>
2020-03-27 17:45:29 +0100

    Fix VertxRouter setup.

--- 08-kogito-travel-agency/src/main/java/org/kie/kogito/app/VertxRouter.java
+++ 08-kogito-travel-agency/src/main/java/org/kie/kogito/app/VertxRouter.java
@@ -1,13 +1,18 @@
 package org.kie.kogito.app;
 
+import java.net.URL;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import io.quarkus.vertx.web.Route;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpHeaders;
-import io.vertx.ext.web.RoutingContext;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.FaviconHandler;
+import io.vertx.ext.web.handler.LoggerHandler;
+import io.vertx.ext.web.handler.StaticHandler;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import static io.vertx.core.http.HttpMethod.GET;
@@ -27,31 +32,25 @@ public class VertxRouter {
     @Inject
     Vertx vertx;
 
-    private String resource;
+    private Buffer resource;
 
     @PostConstruct
     public void init() {
         try {
-            resource = vertx.fileSystem()
-                    .readFileBlocking("META-INF/resources/index.html")
-                    .toString(UTF_8)
-                    .replace("__GRAPHQL_HTTP_ENDPOINT__", "\"" + dataIndexHttpURL + "/graphql\"")
-                    .replace("__GRAPHQL_WS_ENDPOINT__", "\"" + dataIndexWsURL + "/graphql\"");
+            resource = Buffer.buffer(vertx.fileSystem()
+                                             .readFileBlocking("META-INF/resources/index.html")
+                                             .toString(UTF_8)
+                                             .replace("__GRAPHQL_HTTP_ENDPOINT__", "\"" + dataIndexHttpURL + "/graphql\"")
+                                             .replace("__GRAPHQL_WS_ENDPOINT__", "\"" + dataIndexWsURL + "/graphql\""));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    @Route(path = "/", methods = GET)
-    public void handle(RoutingContext context) {
-        try {
-            context.response()
-                    .putHeader(HttpHeaders.CACHE_CONTROL, "no-cache")
-                    .putHeader(HttpHeaders.CONTENT_TYPE, "text/html;charset=utf8")
-                    .end(resource);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            context.fail(500, ex);
-        }
+    void setupRouter(@Observes Router router) {
+        router.route().handler(LoggerHandler.create());
+        router.route().handler(FaviconHandler.create());
+        router.route().handler(StaticHandler.create());
+        router.route(GET, "/").handler(ctx -> ctx.response().end(resource));
     }
 }
