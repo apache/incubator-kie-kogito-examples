47c44546b644b02aa8ed809c035ce15a9bf3cbfc
Tiago Dolphine <tiagodolphine@gmail.com>
2020-04-08 02:30:15 -0300

    Fix Travel agency fixed versions on Pom, causing erros on native build and fix startDataIndex.sh script (#186)
    fix gitignore

--- kogito-travel-agency/scripts/startManagementConsole.sh
+++ kogito-travel-agency/scripts/startManagementConsole.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Starting the Kogito Management Console"
 
-MANAGEMENT_CONSOLE_VERSION=0.8.1
+MANAGEMENT_CONSOLE_VERSION=0.9.0
 MANAGEMENT_CONSOLE_RUNNER=https://search.maven.org/remotecontent?filepath=org/kie/kogito/management-console/${MANAGEMENT_CONSOLE_VERSION}/management-console-${MANAGEMENT_CONSOLE_VERSION}-runner.jar
 
 wget -nc -O management-console-${MANAGEMENT_CONSOLE_VERSION}-runner.jar ${MANAGEMENT_CONSOLE_RUNNER}
