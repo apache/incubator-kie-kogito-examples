aa410855cccd99afeb9b69f11a6ed596ed8cc595
Karel Suta <ksuta@redhat.com>
2020-04-14 15:27:40 +0200

    [KOGITO-1864] Adjust process-kafka-quickstart-springboot example for Kogito operator
    Signed-off-by: Karel Suta <ksuta@redhat.com>

--- process-kafka-quickstart-springboot/src/main/java/org/kie/kogito/tests/KafkaConsumerConfig.java
+++ process-kafka-quickstart-springboot/src/main/java/org/kie/kogito/tests/KafkaConsumerConfig.java
@@ -17,12 +17,12 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @Configuration
 public class KafkaConsumerConfig {
 
-    @Value(value = "${kafka.bootstrapAddress}")
-    private String bootstrapAddress;
+    @Value(value = "${kafka.bootstrap.servers}")
+    private String bootstrapServers;
 
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "travellers-group");
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new StringDeserializer());
     }
@@ -31,6 +31,8 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        // Can be removed when spring-kafka version 2.3.4 and later is used as the missingTopicsFatal is set to false by default since that version
+        factory.setMissingTopicsFatal(false);
         return factory;
     }
 
--- process-kafka-quickstart-springboot/src/main/java/org/kie/kogito/tests/KafkaProducerConfig.java
+++ process-kafka-quickstart-springboot/src/main/java/org/kie/kogito/tests/KafkaProducerConfig.java
@@ -16,15 +16,15 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Value(value = "${kafka.bootstrapAddress}")
-    private String bootstrapAddress;
+    @Value(value = "${kafka.bootstrap.servers}")
+    private String bootstrapServers;
 
     
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);        
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
