76cb164f0f29ecd0a60dfcd727cfc8b83886f38b
Jiri Petrlik <jiripetrlik@gmail.com>
2020-06-03 13:03:48 +0200

    [FAI-191] Implemente test for Grafana dashboards in Kogito metrics

--- dmn-drools-quarkus-metrics/docker-compose/run-compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ../
-mvn clean package
-
-cd docker-compose/ 
-cp ../target/generated-resources/kogito/dashboards/* grafana/provisioning/dashboards
-
-docker-compose build && docker-compose up
