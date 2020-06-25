fd4ab7e23d4fed8c1db75360de71338a60d95a77
Jacopo Rota <jacopo.r00ta@gmail.com>
2020-06-25 16:09:03 +0200

    Update MyInterceptor.java

--- dmn-drools-quarkus-metrics/src/main/java/org/kie/kogito/examples/MyInterceptor.java
+++ dmn-drools-quarkus-metrics/src/main/java/org/kie/kogito/examples/MyInterceptor.java
@@ -12,7 +12,8 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- */package org.kie.kogito.examples;
+ */
+package org.kie.kogito.examples;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
