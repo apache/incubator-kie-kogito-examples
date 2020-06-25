ef72690f3c09cc1854f18ca8b21c1e75f627006e
jake <jacopo.r00ta@gmail.com>
2020-06-25 14:21:58 +0200

    fix readme, optimize import and code, fix licence

--- dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/GrafanaDockerComposeIT.java
+++ dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/GrafanaDockerComposeIT.java
@@ -1,23 +1,22 @@
 /**
- *  Copyright 2020 Red Hat, Inc. and/or its affiliates.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.kie.kogito.examples;
 
-import static org.hamcrest.Matchers.hasItem;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.File;
 
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
@@ -25,10 +24,9 @@ import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.arrayContaining;
 
-import java.io.File;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
 
 @Testcontainers
 public class GrafanaDockerComposeIT {
@@ -41,7 +39,7 @@ public class GrafanaDockerComposeIT {
             new DockerComposeContainer(new File("./docker-compose.yml"))
                     .withExposedService("grafana_1", 3000, Wait.forHttp("/")
                             .forStatusCode(200));
-    
+
     @Test
     public void testPrometheusDataSource() {
         given()
--- dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/LoanEligibilityTest.java
+++ dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/LoanEligibilityTest.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright 2020 Red Hat, Inc. and/or its affiliates.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kie.kogito.examples;
 
@@ -30,12 +30,12 @@ public class LoanEligibilityTest {
     public void testEvaluateTrafficViolation() {
         given()
                 .body("{" +
-                          "\"Client\": " +
-                            "{\"age\": 43,\"salary\": 1950,\"existing payments\": 100}," +
-                            "\"Loan\": {\"duration\": 15,\"installment\": 180}, " +
-                            "\"SupremeDirector\" : \"Yes\", " +
-                            "\"Bribe\": 1000" +
-                          "}"
+                              "\"Client\": " +
+                              "{\"age\": 43,\"salary\": 1950,\"existing payments\": 100}," +
+                              "\"Loan\": {\"duration\": 15,\"installment\": 180}, " +
+                              "\"SupremeDirector\" : \"Yes\", " +
+                              "\"Bribe\": 1000" +
+                              "}"
                 )
                 .contentType(ContentType.JSON)
                 .when()
