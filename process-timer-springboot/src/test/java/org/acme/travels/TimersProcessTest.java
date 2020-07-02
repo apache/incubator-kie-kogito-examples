65e84bb36e5235c49b31212760afa065f469eb69
Daniele Zonca <dzonca@redhat.com>
2020-07-02 12:06:23 +0200

    [KOGITO-2627] Remove unnecessary cast in kogito-examples tests to register listeners (#285)

--- process-timer-springboot/src/test/java/org/acme/travels/TimersProcessTest.java
+++ process-timer-springboot/src/test/java/org/acme/travels/TimersProcessTest.java
@@ -15,25 +15,24 @@
  */
 package org.acme.travels;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kie.kogito.Application;
 import org.kie.kogito.Model;
 import org.kie.kogito.process.Process;
 import org.kie.kogito.process.ProcessInstance;
-import org.kie.kogito.process.impl.CachedProcessEventListenerConfig;
 import org.kie.kogito.tests.KogitoInfinispanSpringbootApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -50,7 +49,7 @@ public class TimersProcessTest {
     @Test
     public void testTimersProcess() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
-        ((CachedProcessEventListenerConfig) app.config().process().processEventListeners()).register(new CompleteProcessListener(latch));
+        app.config().process().processEventListeners().listeners().add(new CompleteProcessListener(latch));
 
         Process<? extends Model> timersProcess = app.processes().processById("timers");
         assertNotNull(timersProcess);
