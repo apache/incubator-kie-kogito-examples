13f05072c124cbedf28ba8d814d1d5b406dbd93f
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-07-06 10:31:37 +0200

    KOGITO-2654 Travel Agency Basic is using an old version of Kogito

--- kogito-travel-agency/basic/src/main/java/org/kie/kogito/app/RuleEventListenerConfig.java
+++ kogito-travel-agency/basic/src/main/java/org/kie/kogito/app/RuleEventListenerConfig.java
@@ -17,8 +17,7 @@
 package org.kie.kogito.app;
 
 import org.drools.core.config.DefaultRuleEventListenerConfig;
-import org.kie.addons.monitoring.rule.PrometheusMetricsDroolsListener;
-
+import org.kie.kogito.monitoring.rule.PrometheusMetricsDroolsListener;
 
 public class RuleEventListenerConfig extends DefaultRuleEventListenerConfig {
 
