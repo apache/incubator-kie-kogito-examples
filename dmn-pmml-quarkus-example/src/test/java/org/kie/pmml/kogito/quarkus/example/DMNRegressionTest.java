5a26e64a3b49b73eed62c2620e2db52e6ac3ea2a
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2020-07-22 10:45:45 +0200

    [DROOLS-5491] Implemented dmn-pmml-quarkus tests

---
+++ dmn-pmml-quarkus-example/src/test/java/org/kie/pmml/kogito/quarkus/example/DMNRegressionTest.java
@@ -0,0 +1,58 @@
+/**
+ *  Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.kie.pmml.kogito.quarkus.example;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class DMNRegressionTest {
+    
+    @Test
+    public void testEvaluateRegressionDMN() {
+        String inputData = "{\"fld1\":3.0, \"fld2\":2.0, \"fld3\":\"y\"}";
+        given()
+                .body(inputData)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/TestRegressionDMN")
+                .then()
+                .statusCode(200);
+//        Object resultVariables =  given()
+//                .contentType(ContentType.JSON)
+//                .body(inputData)
+//                .when()
+//                .post("/TestRegressionDMN")
+//                .then()
+//                .statusCode(200)
+//                .body("correlationId", is(new IsNull()))
+//                .body("segmentationId", is(new IsNull()))
+//                .body("segmentId", is(new IsNull()))
+//                .body("segmentIndex", is(0)) // as JSON is not schema aware, here we assert the RAW string
+//                .body("resultCode", is("OK"))
+//                .body("resultObjectName", is("fld4"))
+//                .extract()
+//                .path("resultVariables");
+//        assertNotNull(resultVariables);
+//        assertTrue(resultVariables instanceof Map);
+//        Map<String, Object> mappedResultVariables = (Map) resultVariables;
+//        assertTrue(mappedResultVariables.containsKey("fld4"));
+//        assertEquals(52.5f, mappedResultVariables.get("fld4"));
+    }
+}
