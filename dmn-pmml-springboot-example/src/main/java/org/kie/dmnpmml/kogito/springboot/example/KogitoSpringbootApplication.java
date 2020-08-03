211d8fb3412145830f9594a98c9b4ef844497619
Jose <josecarvajalhilario@gmail.com>
2020-08-03 09:21:21 +0200

    KOGITO-2704: Use random ports for testcontainers in Kogito Examples

--- dmn-pmml-springboot-example/src/main/java/org/kie/dmnpmml/kogito/springboot/example/KogitoSpringbootApplication.java
+++ dmn-pmml-springboot-example/src/main/java/org/kie/dmnpmml/kogito/springboot/example/KogitoSpringbootApplication.java
@@ -18,10 +18,10 @@ package org.kie.dmnpmml.kogito.springboot.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"org.kie.dmnpmml.kogito.**", "org.kie.kogito.app.**"})
+@SpringBootApplication(scanBasePackages = {"org.kie.dmnpmml.kogito.**", "org.kie.kogito.app.**"})
 public class KogitoSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KogitoSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KogitoSpringbootApplication.class, args);
+    }
 }
