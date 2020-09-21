4ebb7ed588c98ce6cdd918be087d9cb912193b08
ruromero <rromerom@redhat.com>
2020-09-21 13:06:38 +0200

    [KOGITO-3297] Update ApplicantResource
    Signed-off-by: ruromero <rromerom@redhat.com>

--- serverless-workflow-events-quarkus/src/main/java/org/kogito/serverless/examples/ApplicantResource.java
+++ serverless-workflow-events-quarkus/src/main/java/org/kogito/serverless/examples/ApplicantResource.java
@@ -15,27 +15,33 @@
  */
 package org.kogito.serverless.examples;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import io.cloudevents.json.Json;
-import io.cloudevents.v1.CloudEventBuilder;
-import io.cloudevents.v1.CloudEventImpl;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-
+import java.net.URI;
+import java.util.UUID;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.net.URI;
-import java.util.Random;
-import java.util.UUID;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/newapplicant")
 public class ApplicantResource {
 
-    Random rand = new Random();
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicantResource.class);
+
+    @Inject
+    ObjectMapper mapper;
 
     @Inject
     @Channel("out-applicants")
@@ -45,15 +51,17 @@ public class ApplicantResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public void submitApplicant(JsonNode newApplicant) {
-        CloudEventImpl<JsonNode> applicantEvent =
-                CloudEventBuilder.<JsonNode>builder()
-                        .withId(UUID.randomUUID().toString())
-                        .withType("newApplicantEvent")
-                        .withSource(URI.create("http://localhost:8080"))
-                        .withData(newApplicant)
-                        .build();
-
-        newApplicantEmitter.send(Json.encode(applicantEvent));
-
+        try {
+            CloudEvent applicantEvent = CloudEventBuilder.v1()
+                    .withId(UUID.randomUUID().toString())
+                    .withType("newApplicantEvent")
+                    .withSource(URI.create("http://localhost:8080"))
+                    .withData(mapper.writeValueAsBytes(newApplicant))
+                    .build();
+            newApplicantEmitter.send(mapper.writeValueAsString(applicantEvent));
+        } catch (JsonProcessingException e) {
+            LOGGER.error("Unable to process cloud event", e);
+            throw new InternalServerErrorException("Unable to write cloud event data", e);
+        }
     }
 }
