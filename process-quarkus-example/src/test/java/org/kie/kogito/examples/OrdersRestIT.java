b8a2c871b3f1bf380d29e2e6008657df85422a1c
jake <jacopo.r00ta@gmail.com>
2020-10-21 10:16:20 +0200

    update tests

--- process-quarkus-example/src/test/java/org/kie/kogito/examples/OrdersRestIT.java
+++ process-quarkus-example/src/test/java/org/kie/kogito/examples/OrdersRestIT.java
@@ -1,21 +1,19 @@
 /**
- *  Copyright 2020 Red Hat, Inc. and/or its affiliates.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kie.kogito.examples;
-
-import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -35,7 +33,6 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
@@ -46,7 +43,7 @@ public class OrdersRestIT {
     @Inject
     @Named("demo.orders")
     Process<? extends Model> orderProcess;
-    
+
     @Inject
     @Named("demo.orderItems")
     Process<? extends Model> orderItemsProcess;
@@ -65,13 +62,13 @@ public class OrdersRestIT {
         // test adding new order
         String addOrderPayload = "{\"approver\" : \"john\", \"order\" : {\"orderNumber\" : \"12345\", \"shipped\" : false}}";
         String firstCreatedId = given()
-                    .contentType(ContentType.JSON)
-                    .accept(ContentType.JSON)
-                    .body(addOrderPayload).when()
-                    .post("/orders")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(addOrderPayload).when()
+                .post("/orders")
                 .then()
-                    .statusCode(201)
-                    .header("Location", notNullValue())
+                .statusCode(201)
+                .header("Location", notNullValue())
                 .extract().path("id");
 
         // test getting the created order
@@ -81,9 +78,9 @@ public class OrdersRestIT {
         // test getting order by id
         given()
                 .accept(ContentType.JSON)
-            .when()
+                .when()
                 .get("/orders/{id}", firstCreatedId)
-            .then()
+                .then()
                 .statusCode(200)
                 .body("id", is(firstCreatedId));
 
@@ -91,48 +88,48 @@ public class OrdersRestIT {
         // first add second order...
         String secondCreatedId =
                 given()
-                    .contentType(ContentType.JSON)
-                    .accept(ContentType.JSON)
-                    .body(addOrderPayload)
-                .when()
-                    .post("/orders")
-                .then()
-                    .statusCode(201)
-                .extract()
-                    .path("id");
+                        .contentType(ContentType.JSON)
+                        .accept(ContentType.JSON)
+                        .body(addOrderPayload)
+                        .when()
+                        .post("/orders")
+                        .then()
+                        .statusCode(201)
+                        .extract()
+                        .path("id");
         // now delete the first order created
         given()
                 .accept(ContentType.JSON)
-            .when()
+                .when()
                 .delete("/orders/{id}", firstCreatedId)
-            .then()
+                .then()
                 .statusCode(200);
         // get all orders make sure there is only one
         given()
                 .accept(ContentType.JSON)
-            .when()
+                .when()
                 .get("/orders")
-            .then()
+                .then()
                 .statusCode(200)
                 .body("$.size()", is(1), "[0].id", is(secondCreatedId));
 
         // delete second before finishing
         given()
                 .accept(ContentType.JSON)
-            .when()
+                .when()
                 .delete("/orders/{id}", secondCreatedId)
-            .then()
+                .then()
                 .statusCode(200);
         // get all orders make sure there is zero
         given()
                 .accept(ContentType.JSON)
-            .when()
+                .when()
                 .get("/orders")
-            .then()
+                .then()
                 .statusCode(200)
                 .body("$.size()", is(0));
     }
-    
+
     @Test
     public void testOrdersWithErrorRest() {
         assertNotNull(orderProcess);
@@ -144,38 +141,38 @@ public class OrdersRestIT {
                 .accept(ContentType.JSON)
                 .body(addOrderPayload).when()
                 .post("/orders")
-            .then()
+                .then()
                 .statusCode(500)
                 .body("id", notNullValue())
-            .extract()
+                .extract()
                 .path("id");
 
         // test getting the created order
         given().accept(ContentType.JSON).when().get("/orders").then().statusCode(200)
                 .body("$.size()", is(1), "[0].id", is(firstCreatedId));
-        
+
         // test retrieving error info using process management addon
         given().accept(ContentType.JSON).when().get("/management/processes/demo.orders/instances/" + firstCreatedId + "/error").then()
-        .statusCode(200).body("id", is(firstCreatedId));
-        
+                .statusCode(200).body("id", is(firstCreatedId));
+
         String fixedOrderPayload = "{\"approver\" : \"john\", \"order\" : {\"orderNumber\" : \"12345\", \"shipped\" : false}}";
         given().contentType(ContentType.JSON).accept(ContentType.JSON).body(fixedOrderPayload).when().put("/orders/" + firstCreatedId).then()
-        .statusCode(200)
+                .statusCode(200)
                 .body("id", is(firstCreatedId))
                 .body("approver", equalTo("john"))
                 .body("order.orderNumber", equalTo("12345"))
                 .body("order.shipped", equalTo(false));
-        
+
         given().accept(ContentType.JSON).when().post("/management/processes/demo.orders/instances/" + firstCreatedId + "/retrigger").then()
-        .statusCode(200);
-        
+                .statusCode(200);
+
         // delete second before finishing
         given().accept(ContentType.JSON).when().delete("/orders/" + firstCreatedId).then().statusCode(200);
         // get all orders make sure there is zero
         given().accept(ContentType.JSON).when().get("/orders").then().statusCode(200)
                 .body("$.size()", is(0));
     }
-    
+
     @Test
     public void testOrdersWithOrderItemsRest() {
         assertNotNull(orderProcess);
@@ -196,44 +193,44 @@ public class OrdersRestIT {
         // test getting order items subprocess
         String orderItemsId = given().accept(ContentType.JSON).when().get("/orderItems").then().statusCode(200)
                 .body("$.size()", is(1)).extract().path("[0].id");
-        
+
         // test getting order items by id
         given().accept(ContentType.JSON).when().get("/orderItems/" + orderItemsId).then()
                 .statusCode(200).body("id", is(orderItemsId));
-        
+
         // test getting task
         String taskId = given()
-            .accept(ContentType.JSON)
-            .when()
-            .get("/orderItems/" + orderItemsId + "/tasks?user=john")
-            .then()
-            .statusCode(200)
-            .body("$.size", is(1))
-            .body("[0].name", is("Verify order"))
-            .extract()
-            .path("[0].id");
-        
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orderItems/" + orderItemsId + "/tasks?user=john")
+                .then()
+                .statusCode(200)
+                .body("$.size", is(1))
+                .body("[0].name", is("Verify order"))
+                .extract()
+                .path("[0].id");
+
         // test completing task
         String payload = "{}";
         given()
-            .contentType(ContentType.JSON)
-            .accept(ContentType.JSON)
-            .body(payload)
-            .when()
-            .post("/orderItems/" + orderItemsId + "/Verify_order/" + taskId + "?user=john")
-            .then()
-            .statusCode(200)
-            .body("id", is(orderItemsId));
-        
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/orderItems/" + orderItemsId + "/Verify_order/" + taskId + "?user=john")
+                .then()
+                .statusCode(200)
+                .body("id", is(orderItemsId));
+
         // get all orders make sure there is zero
         given().accept(ContentType.JSON).when().get("/orders").then().statusCode(200)
                 .body("$.size()", is(0));
-        
+
         // get all order items make sure there is zero
         given().accept(ContentType.JSON).when().get("/orderItems").then().statusCode(200)
                 .body("$.size()", is(0));
     }
-    
+
     @Test
     public void testOrdersWithOrderItemsAbortedRest() {
         assertNotNull(orderProcess);
@@ -254,29 +251,28 @@ public class OrdersRestIT {
         // test getting order items subprocess
         String orderItemsId = given().accept(ContentType.JSON).when().get("/orderItems?businessKey=ORD-0001").then().statusCode(200)
                 .body("$.size()", is(1)).extract().path("[0].id");
-        
+
         // test getting order items by id
         given().accept(ContentType.JSON).when().get("/orderItems/" + orderItemsId).then()
                 .statusCode(200).body("id", is(orderItemsId));
-        
+
         // test getting task
         given()
-            .accept(ContentType.JSON)
-            .when()
-            .get("/orderItems/" + orderItemsId + "/tasks?user=john")
-            .then()
-            .statusCode(200)
-            .body("$.size", is(1))
-            .body("[0].name", is("Verify order"));
-        
-        
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orderItems/" + orderItemsId + "/tasks?user=john")
+                .then()
+                .statusCode(200)
+                .body("$.size", is(1))
+                .body("[0].name", is("Verify order"));
+
         // test deleting order items
         given().accept(ContentType.JSON).when().delete("/orderItems/" + orderItemsId).then().statusCode(200);
-        
+
         // get all orders make sure there is zero
         given().accept(ContentType.JSON).when().get("/orders").then().statusCode(200)
                 .body("$.size()", is(0));
-        
+
         // get all order items make sure there is zero
         given().accept(ContentType.JSON).when().get("/orderItems").then().statusCode(200)
                 .body("$.size()", is(0));
@@ -301,29 +297,21 @@ public class OrdersRestIT {
         // update the instance
         orderPayload = "{\"approver\" : \"joe\", \"order\" : {\"orderNumber\" : \"54321\", \"shipped\" : true}}";
         given().contentType(ContentType.JSON).accept(ContentType.JSON).body(orderPayload).when()
-        .put("/orders/{id}", id).then()
+                .put("/orders/{id}", id).then()
                 .statusCode(200)
                 .body("approver", equalTo("joe"))
                 .body("order.orderNumber", equalTo("54321"))
                 .body("order.shipped", equalTo(true));
-        
+
         // get all orders make sure there is one
         given().accept(ContentType.JSON).when().get("/orders").then().statusCode(200)
                 .body("$.size()", is(1));
-        
+
         // test deleting order items by custom ID
         given().accept(ContentType.JSON).when().delete("/orders/{id}", id).then().statusCode(200);
-        
+
         // get all orders make sure there is zero
         given().accept(ContentType.JSON).when().get("/orders").then().statusCode(200)
                 .body("$.size()", is(0));
-    }
-
-    @Test
-    public void testProtobufListIsAvailable() {
-        List<String> files = given().contentType(ContentType.JSON).accept(ContentType.JSON).when()
-                .get("/persistence/protobuf/list.json").as(List.class);
-
-        assertEquals(2, files.size());
     }
 }
