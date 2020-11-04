4dabf5b03da747a18d2ab27e28c053f9f60d2402
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2020-11-04 19:22:22 -0300

    [KOGITO-3610] Examples update to reflect the new Topics info endpoint (#424)
    * [KOGITO-3610] Examples update to reflect the new Topics info endpoint
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    * Adding CloudEvents for Quarkus
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    * Fixing unit tests
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- process-knative-quickstart-quarkus/src/test/java/org/acme/travel/TopicsInformationTest.java
+++ process-knative-quickstart-quarkus/src/test/java/org/acme/travel/TopicsInformationTest.java
@@ -17,12 +17,15 @@
 package org.acme.travel;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.kie.kogito.event.CloudEventMeta;
+import org.kie.kogito.event.EventKind;
 import org.kie.kogito.event.Topic;
-import org.kie.kogito.event.TopicType;
+import org.kie.kogito.event.ChannelType;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItem;
@@ -34,9 +37,14 @@ public class TopicsInformationTest {
 
     @Test
     void verifyTopicsInformation() {
+        final Topic expectedIncomingTopic = new Topic("kogito_incoming_stream", ChannelType.INCOMING);
+        expectedIncomingTopic.setEventsMeta(Collections.singletonList(new CloudEventMeta("travellers", "", EventKind.CONSUMED)));
+        final Topic expectedOutgoingTopic = new Topic("processedtravellers", ChannelType.OUTGOING);
+        expectedOutgoingTopic.setEventsMeta(Collections.singletonList(new CloudEventMeta("process.travelers.processedtravellers", "/process/travelers", EventKind.PRODUCED)));
+
         final List<Topic> topics = Arrays.asList(given().get("/messaging/topics").as(Topic[].class));
         assertThat(topics, notNullValue());
-        assertThat(topics, hasItem(new Topic("travellers", TopicType.CONSUMED)));
-        assertThat(topics, hasItem(new Topic("processedtravellers", TopicType.PRODUCED)));
+        assertThat(topics, hasItem(expectedIncomingTopic));
+        assertThat(topics, hasItem(expectedOutgoingTopic));
     }
 }
