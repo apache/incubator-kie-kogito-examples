79ad7c2b9e982ae8cdfa7fe608a20fdeeddb4200
Tiago Dolphine <tiagodolphine@gmail.com>
2020-11-09 11:29:07 -0300

    fixing code style in file headers

--- process-saga-quarkus/src/main/java/org/kie/kogito/MockService.java
+++ process-saga-quarkus/src/main/java/org/kie/kogito/MockService.java
@@ -1,4 +1,4 @@
-/*
+/**
  *  Copyright 2020 Red Hat, Inc. and/or its affiliates.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.kie.kogito;
 
 import java.util.Objects;
