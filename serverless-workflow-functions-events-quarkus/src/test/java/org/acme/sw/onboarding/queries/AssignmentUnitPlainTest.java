c1ad1d9345ea1dcee487da8a0be0255390bda31f
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-11-11 10:25:03 +0100

    proper assertion in plain test

--- serverless-workflow-functions-events-quarkus/src/test/java/org/acme/sw/onboarding/queries/AssignmentUnitPlainTest.java
+++ serverless-workflow-functions-events-quarkus/src/test/java/org/acme/sw/onboarding/queries/AssignmentUnitPlainTest.java
@@ -1,14 +1,21 @@
 package org.acme.sw.onboarding.queries;
 
+import java.util.ArrayList;
+
+import org.acme.sw.onboarding.model.Doctor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.kie.kogito.rules.DataObserver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AssignmentUnitPlainTest {
     @Test
     public void create() {
         AssignmentUnit assignmentUnit = new AssignmentUnit();
-        assignmentUnit.getDoctors().subscribe(DataObserver.of(System.out::println));
+        ArrayList<Doctor> doctors = new ArrayList<>();
+        assignmentUnit.getDoctors().subscribe(DataObserver.of(doctors::add));
+        assertEquals(doctors.size(), 6);
     }
 
 }
