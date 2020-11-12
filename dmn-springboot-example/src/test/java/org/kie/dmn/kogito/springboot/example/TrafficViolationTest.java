7367d39f664f8dace1dbdf9630c2775690f62d11
Tiago Dolphine <tiagodolphine@gmail.com>
2020-11-12 15:15:16 -0300

    fix trafficViolation test endpoint

--- dmn-springboot-example/src/test/java/org/kie/dmn/kogito/springboot/example/TrafficViolationTest.java
+++ dmn-springboot-example/src/test/java/org/kie/dmn/kogito/springboot/example/TrafficViolationTest.java
@@ -54,7 +54,7 @@ public class TrafficViolationTest {
                      "}")
                .contentType(ContentType.JSON)
           .when()
-               .post("/Traffic%20Violation")
+               .post("/Traffic Violation")
           .then()
              .statusCode(200)
                .body("'Should the driver be suspended?'", is("No"));
