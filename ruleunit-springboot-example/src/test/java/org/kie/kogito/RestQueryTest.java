39e83da0c26a365197ae5d4b17d210399acc2cb6
Tiago Dolphine <tiagodolphine@gmail.com>
2020-11-26 11:06:24 -0300

    ajusting tests for rules

--- ruleunit-springboot-example/src/test/java/org/kie/kogito/RestQueryTest.java
+++ ruleunit-springboot-example/src/test/java/org/kie/kogito/RestQueryTest.java
@@ -15,23 +15,31 @@
  */
 package org.kie.kogito;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.kie.kogito.queries.KogitoSpringbootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = KogitoSpringbootApplication.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoSpringbootApplication.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class RestQueryTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
 
     private static final String JSON_PAYLOAD =
             "{\n" +
