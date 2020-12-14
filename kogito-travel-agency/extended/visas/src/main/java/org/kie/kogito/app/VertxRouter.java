10f79e059c8189729cbfddcd2eea81090deaab04
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2020-12-15 01:35:45 +1000

    KOGITO-3994 Use Qute templating to handle Data Index config

--- kogito-travel-agency/extended/visas/src/main/java/org/kie/kogito/app/VertxRouter.java
+++ kogito-travel-agency/extended/visas/src/main/java/org/kie/kogito/app/VertxRouter.java
@@ -15,55 +15,29 @@
  */
 package org.kie.kogito.app;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 
-import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.api.ResourcePath;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.FaviconHandler;
 import io.vertx.ext.web.handler.LoggerHandler;
 import io.vertx.ext.web.handler.StaticHandler;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import static io.vertx.core.http.HttpMethod.GET;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 @ApplicationScoped
 public class VertxRouter {
 
-    @Inject
-    @ConfigProperty(name = "kogito.dataindex.http.url", defaultValue = "http://localhost:8180")
-    String dataIndexHttpURL;
-
-    @Inject
-    @ConfigProperty(name = "kogito.dataindex.ws.url", defaultValue = "ws://localhost:8180")
-    String dataIndexWsURL;
-
-    @Inject
-    Vertx vertx;
-
-    private Buffer resource;
-
-    @PostConstruct
-    public void init() {
-        try {
-            resource = Buffer.buffer(vertx.fileSystem()
-                                             .readFileBlocking("META-INF/resources/index.html")
-                                             .toString(UTF_8)
-                                             .replace("__GRAPHQL_HTTP_ENDPOINT__", "\"" + dataIndexHttpURL + "/graphql\"")
-                                             .replace("__GRAPHQL_WS_ENDPOINT__", "\"" + dataIndexWsURL + "/graphql\""));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+    @ResourcePath("index")
+    Template indexTemplate;
 
     void setupRouter(@Observes Router router) {
         router.route().handler(LoggerHandler.create());
         router.route().handler(FaviconHandler.create());
         router.route().handler(StaticHandler.create());
-        router.route(GET, "/").handler(ctx -> ctx.response().end(resource));
+        String indexPage = indexTemplate.render();
+        router.route(GET, "/").handler(ctx -> ctx.response().end(indexPage));
     }
 }
