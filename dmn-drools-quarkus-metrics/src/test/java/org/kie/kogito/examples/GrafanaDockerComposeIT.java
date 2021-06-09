e3289c860d8970af8ec637cb0f6f2005402b5d97
Jacopo Rota <jacopo.r00ta@gmail.com>
2021-06-09 14:32:44 +0200

    update

--- dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/GrafanaDockerComposeIT.java
+++ dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/GrafanaDockerComposeIT.java
@@ -23,7 +23,6 @@ import java.util.Properties;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.kie.kogito.testcontainers.Constants;
