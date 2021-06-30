0ba2261c8cadaad08330ba2bbd43789cea213b40
Jozef Marko <jomarko@redhat.com>
2021-06-30 13:53:59 +0200

    KOGITO-5447 move tests with non unique FQN

---
+++ process-service-calls-quarkus/src/test/java/org/acme/travels/quarkus/MultiParamServiceProcessTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.acme.travels.quarkus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.junit.jupiter.api.Test;
+import org.kie.kogito.Model;
+import org.kie.kogito.process.Process;
+import org.kie.kogito.process.ProcessInstance;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@QuarkusTest
+public class MultiParamServiceProcessTest {
+
+    @Named("multiparams")
+    @Inject
+    Process<? extends Model> multiparamsProcess;
+
+    @Test
+    public void testServiceWithMultipleParams() {
+
+        assertNotNull(multiparamsProcess);
+
+        Model m = multiparamsProcess.createModel();
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", "john");
+        parameters.put("age", 44);
+        m.fromMap(parameters);
+
+        ProcessInstance<?> processInstance = multiparamsProcess.createInstance(m);
+        processInstance.start();
+        assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED, processInstance.status());
+        Model result = (Model) processInstance.variables();
+        assertEquals(2, result.toMap().size());
+
+    }
+}
---
+++ process-service-calls-quarkus/src/test/java/org/acme/travels/quarkus/TravellersProcessTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.acme.travels.quarkus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.junit.jupiter.api.Test;
+import org.kie.kogito.Model;
+import org.kie.kogito.process.Process;
+import org.kie.kogito.process.ProcessInstance;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@QuarkusTest
+public class TravellersProcessTest {
+
+    @Named("travellers")
+    @Inject
+    Process<? extends Model> travellersProcess;
+
+    @Test
+    public void testNewTraveller() {
+
+        assertNotNull(travellersProcess);
+
+        Model m = travellersProcess.createModel();
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("traveller", new Traveller("John", "Doe", "john.doe@example.com", "American", new Address("main street", "Boston", "10005", "US")));
+        m.fromMap(parameters);
+
+        ProcessInstance<?> processInstance = travellersProcess.createInstance(m);
+        processInstance.start();
+        assertEquals(org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED, processInstance.status());
+        Model result = (Model) processInstance.variables();
+        assertEquals(2, result.toMap().size());
+        assertEquals(result.toMap().get("stored"), true);
+    }
+}
