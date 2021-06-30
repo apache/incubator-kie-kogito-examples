635fc90a80394a2b1cd920dee1c7b4c11c9559d1
Jozef Marko <jomarko@redhat.com>
2021-06-30 13:53:59 +0200

    KOGITO-5447 move tests with non unique FQN

---
+++ dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/quarkus/LoanEligibilityTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.examples.quarkus;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+public class LoanEligibilityTest {
+
+    @Test
+    public void testEvaluateLoanEligibility() {
+        // Approved Loan
+        given()
+                .body("{" +
+                        "\"Client\": " +
+                        "{\"age\": 43,\"salary\": 1950,\"existing payments\": 100}," +
+                        "\"Loan\": {\"duration\": 15,\"installment\": 180}, " +
+                        "\"SupremeDirector\" : \"Yes\", " +
+                        "\"Bribe\": 1000" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/LoanEligibility")
+                .then()
+                .statusCode(200)
+                .body("'Decide'", is(true));
+
+        // Not approved loan
+        given()
+                .body("{" +
+                        "\"Client\": " +
+                        "{\"age\": 43,\"salary\": 1950,\"existing payments\": 100}," +
+                        "\"Loan\": {\"duration\": 15,\"installment\": 180}, " +
+                        "\"SupremeDirector\" : \"No\", " +
+                        "\"Bribe\": 0" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/LoanEligibility")
+                .then()
+                .statusCode(200)
+                .body("'Decide'", is(false));
+
+        given()
+                .when()
+                .get("/metrics")
+                .then()
+                .statusCode(200)
+                .body(containsString("string_dmn_result_total{decision=\"Eligibility\",endpoint=\"LoanEligibility\",identifier=\"Yes\",} 2.0"))
+                .body(containsString("string_dmn_result_total{decision=\"Judgement\",endpoint=\"LoanEligibility\",identifier=\"Yes\",} 1.0"))
+                .body(containsString("string_dmn_result_total{decision=\"Judgement\",endpoint=\"LoanEligibility\",identifier=\"No\",} 1.0"))
+                .body(containsString("boolean_dmn_result_total{decision=\"Decide\",endpoint=\"LoanEligibility\",identifier=\"true\",} 1.0"))
+                .body(containsString("boolean_dmn_result_total{decision=\"Decide\",endpoint=\"LoanEligibility\",identifier=\"false\",} 1.0\n"))
+                .body(containsString("number_dmn_result{decision=\"Is Enough?\",endpoint=\"LoanEligibility\",quantile=\"0.5\",} 0.0"))
+                .body(containsString("number_dmn_result_count{decision=\"Is Enough?\",endpoint=\"LoanEligibility\",} 2.0"))
+                .body(containsString("number_dmn_result_sum{decision=\"Is Enough?\",endpoint=\"LoanEligibility\",} 100.0"))
+                .body(containsString("number_dmn_result{decision=\"Is Enough?\",endpoint=\"LoanEligibility\",quantile=\"0.75\",} 100.0"))
+                .body(containsString("number_dmn_result_max{decision=\"Is Enough?\",endpoint=\"LoanEligibility\",} 100.0"));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testDashboardsListIsAvailable() {
+        List<String> dashboards = given().contentType(ContentType.JSON).accept(ContentType.JSON).when()
+                .get("/monitoring/dashboards/list.json").as(List.class);
+
+        Assertions.assertEquals(4, dashboards.size());
+    }
+}
---
+++ process-usertasks-with-security-quarkus/src/test/java/org/acme/travels/security/quarkus/ApprovalsRestTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.acme.travels.security.quarkus;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@QuarkusTest
+public class ApprovalsRestTest {
+
+    @Test
+    public void testStartApprovalUnauthorized() {
+
+        given()
+                .body("{\"traveller\" : {\"firstName\" : \"John\",\"lastName\" : \"Doe\",\"email\" : \"john.doe@example.com\",\"nationality\" : \"American\",\"address\" : {\"street\" : \"main street\",\"city\" : \"Boston\",\"zipCode\" : \"10005\",\"country\" : \"US\"}}}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/approvals")
+                .then()
+                .statusCode(401);
+
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void testStartApprovalAuthorized() {
+        // start new approval
+        String id = given()
+                .header("Authorization", "Basic am9objpqb2hu")
+                .body("{\"traveller\" : {\"firstName\" : \"John\",\"lastName\" : \"Doe\",\"email\" : \"john.doe@example.com\",\"nationality\" : \"American\",\"address\" : {\"street\" : \"main street\",\"city\" : \"Boston\",\"zipCode\" : \"10005\",\"country\" : \"US\"}}}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/approvals")
+                .then()
+                .statusCode(201)
+                .body("id", notNullValue()).extract().path("id");
+        // get all active approvals
+        given()
+                .header("Authorization", "Basic am9objpqb2hu")
+                .accept(ContentType.JSON)
+                .when()
+                .get("/approvals")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(1), "[0].id", is(id));
+
+        // get just started approval
+        given()
+                .header("Authorization", "Basic am9objpqb2hu")
+                .accept(ContentType.JSON)
+                .when()
+                .get("/approvals/" + id)
+                .then()
+                .statusCode(200)
+                .body("id", is(id));
+
+        // tasks assigned in just started approval
+
+        String taskInfo = given()
+                .header("Authorization", "Basic am9objpqb2hu")
+                .accept(ContentType.JSON)
+                .when()
+                .get("/approvals/" + id + "/tasks?user=admin&group=managers")
+                .then()
+                .statusCode(200)
+                .body("$.size", is(1))
+                .body("[0].name", is("firstLineApproval"))
+                .extract()
+                .path("[0].id");
+
+        // complete first task without authorization header as it authorization is managed on task level
+        // thus user and group(s) must be provided
+        String payload = "{}";
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/approvals/" + id + "/firstLineApproval/" + taskInfo + "?user=mary&group=managers")
+                .then()
+                .statusCode(200)
+                .body("id", is(id));
+
+        // lastly abort the approval
+        given()
+                .header("Authorization", "Basic am9objpqb2hu")
+                .accept(ContentType.JSON)
+                .when()
+                .delete("/approvals/" + id)
+                .then()
+                .statusCode(200)
+                .body("id", is(id));
+    }
+}
