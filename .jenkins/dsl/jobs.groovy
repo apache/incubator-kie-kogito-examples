8f15fbfd67b3d00e2440d1a8e13b59388a9154f0
radtriste <tradisso@redhat.com>
2021-07-19 14:58:55 +0200

    KOGITO-5326 Setup deploy/promote notifications on demand

--- .jenkins/dsl/jobs.groovy
+++ .jenkins/dsl/jobs.groovy
@@ -128,6 +128,8 @@ void setupDeployJob(String jobFolder, KogitoJobType jobType) {
 
             stringParam('PROJECT_VERSION', '', 'Optional if not RELEASE. If RELEASE, cannot be empty.')
             stringParam('OPTAPLANNER_VERSION', '', 'Optional if not RELEASE. If RELEASE, cannot be empty.')
+
+            booleanParam('SEND_NOTIFICATION', false, 'In case you want the pipeline to send a notification on CI channel for this run.')
         }
 
         environmentVariables {
@@ -178,6 +180,8 @@ void setupPromoteJob(String jobFolder, KogitoJobType jobType) {
             stringParam('OPTAPLANNER_VERSION', '', 'Override `deployment.properties`. Optional if not RELEASE. If RELEASE, cannot be empty.')
             stringParam('GIT_TAG', '', 'Git tag to set, if different from PROJECT_VERSION')
             booleanParam('UPDATE_STABLE_BRANCH', false, 'Set to true if you want to update the `stable` branch to new created Git tag.')
+
+            booleanParam('SEND_NOTIFICATION', false, 'In case you want the pipeline to send a notification on CI channel for this run.')
         }
 
         environmentVariables {
