ed25b623a30f5ba67f36ef8a974b3036c0f8d622
Radovan Synek <rsynek@redhat.com>
2021-08-19 01:44:42 +0200

    Disable an unstable test (#839)
    * Disable an unstable test
    
    * Disable the multi messaging test

--- process-kafka-multi-springboot/src/test/java/org/acme/travel/MultiMessagingIT.java
+++ process-kafka-multi-springboot/src/test/java/org/acme/travel/MultiMessagingIT.java
@@ -25,6 +25,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.kie.kogito.test.kafka.KafkaTestClient;
 import org.kie.kogito.testcontainers.springboot.KafkaSpringBootTestResource;
@@ -63,6 +64,7 @@ public class MultiMessagingIT {
     @Autowired
     private KafkaTestClient kafkaClient;
 
+    @Disabled("https://issues.redhat.com/browse/KOGITO-5664")
     @Test
     public void testProcess() throws InterruptedException {
         objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
