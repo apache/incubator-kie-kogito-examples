95e2d3cfad220ec9ec50babf89c8df18f6044c75
Tristan Radisson <tristan.radisson@gmail.com>
2021-09-24 12:41:06 +0200

    [KOGITO-5949] - Removing direct call to remote restcountries web service (#898) (#900)
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    Co-authored-by: Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>

--- serverless-workflow-service-calls-springboot/src/test/java/org/kie/kogito/serverless/examples/CountriesWorkflowIntegrationTest.java
+++ serverless-workflow-service-calls-springboot/src/test/java/org/kie/kogito/serverless/examples/CountriesWorkflowIntegrationTest.java
@@ -36,6 +36,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = ServerlessServiceCallsExampleApplication.class)
 @ExtendWith(SpringExtension.class)
+@ExtendWith(RestCountriesMockServer.class)
 public class CountriesWorkflowIntegrationTest {
 
     @Autowired
