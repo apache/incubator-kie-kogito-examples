1e98fb3035e054729b2ef24daf7ec3046691d30b
Ricardo Zanini <zanini@redhat.com>
2021-10-05 10:44:40 -0300

    [KOGITO-6060] - Fix Temperature Conversion bug and add it to default profile
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- serverless-workflow-temperature-conversion/multiplication-service/src/main/java/org/kie/kogito/examples/sw/temp/multiplication/MultiplicationOperation.java
+++ serverless-workflow-temperature-conversion/multiplication-service/src/main/java/org/kie/kogito/examples/sw/temp/multiplication/MultiplicationOperation.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kie.kogito.examples.sw.temp.subtraction;
+package org.kie.kogito.examples.sw.temp.multiplication;
 
 import java.util.Objects;
 
