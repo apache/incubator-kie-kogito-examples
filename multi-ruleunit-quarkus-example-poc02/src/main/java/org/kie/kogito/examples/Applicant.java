30c0952e45128a3fad18bf3da1762cba6f7adb37
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2021-10-25 17:47:19 +0900

    [KOGITO-6039] [KOGITO-6040] Vert.X example - Applying Vert.X EventBus to LoanApplication 2 RuleUnits example

---
+++ multi-ruleunit-quarkus-example-poc02/src/main/java/org/kie/kogito/examples/Applicant.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.examples;
+
+public class Applicant {
+
+    private String name;
+    private int age;
+    private int creditScore;
+    private boolean invalid = false; // false by default
+
+    public Applicant() {
+    }
+
+    public Applicant(String name, int age, int creditScore) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.creditScore = creditScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public boolean isInvalid() {
+        return invalid;
+    }
+
+    public void setInvalid(boolean invalid) {
+        this.invalid = invalid;
+    }
+
+}
