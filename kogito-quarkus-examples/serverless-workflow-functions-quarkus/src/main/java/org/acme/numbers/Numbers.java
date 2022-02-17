e00fa59972578f621154ed664ff42fac03b7077a
Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
2022-02-17 16:59:57 +0100

    [KOGITO-6775] Using complex parameters (#1131)

--- kogito-quarkus-examples/serverless-workflow-functions-quarkus/src/main/java/org/acme/numbers/Numbers.java
+++ kogito-quarkus-examples/serverless-workflow-functions-quarkus/src/main/java/org/acme/numbers/Numbers.java
@@ -16,11 +16,18 @@
 package org.acme.numbers;
 
 import java.util.Collection;
+import java.util.Map;
 
 public class Numbers {
     private Collection<Number> numbers;
 
+    private Map<String, Object> additionalData;
+
     public Collection<Number> getNumbers() {
         return numbers;
+    }
+
+    public Map<String, Object> getAdditionalData() {
+        return additionalData;
     }
 }
