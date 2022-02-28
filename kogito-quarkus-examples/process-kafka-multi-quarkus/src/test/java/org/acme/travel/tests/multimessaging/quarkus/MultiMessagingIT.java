54280350053be75e29214b93148b31e4ef5119c2
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2022-02-28 11:45:33 +1000

    KOGITO-6825 Review OutboxIT tests (#1142)

--- kogito-quarkus-examples/process-kafka-multi-quarkus/src/test/java/org/acme/travel/tests/multimessaging/quarkus/MultiMessagingIT.java
+++ kogito-quarkus-examples/process-kafka-multi-quarkus/src/test/java/org/acme/travel/tests/multimessaging/quarkus/MultiMessagingIT.java
@@ -24,8 +24,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
-import javax.inject.Inject;
-
 import org.acme.travel.Traveller;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.AfterEach;
@@ -42,15 +40,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.cloudevents.core.builder.CloudEventBuilder;
+import io.cloudevents.jackson.JsonFormat;
 import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-@QuarkusTest
+@QuarkusIntegrationTest
 @QuarkusTestResource(KafkaQuarkusTestResource.class)
 public class MultiMessagingIT {
 
@@ -60,7 +59,6 @@ public class MultiMessagingIT {
 
     private static Logger LOGGER = LoggerFactory.getLogger(MultiMessagingIT.class);
 
-    @Inject
     private ObjectMapper objectMapper;
 
     public KafkaTestClient kafkaClient;
@@ -70,6 +68,8 @@ public class MultiMessagingIT {
 
     @BeforeEach
     public void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(JsonFormat.getCloudEventJacksonModule());
         kafkaClient = new KafkaTestClient(kafkaBootstrapServers);
     }
 
