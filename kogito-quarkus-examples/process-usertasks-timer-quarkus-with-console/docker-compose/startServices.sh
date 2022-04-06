371212f70cf33c767f6cdb014fdb0cdb99e069d0
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2022-04-06 10:46:17 +1000

    KOGITO-6929 - Upgrade Keycloack image (#1209)

--- kogito-quarkus-examples/process-usertasks-timer-quarkus-with-console/docker-compose/startServices.sh
+++ kogito-quarkus-examples/process-usertasks-timer-quarkus-with-console/docker-compose/startServices.sh
@@ -3,6 +3,7 @@
 echo "Script requires your Kogito Quickstart to be compiled"
 
 PROJECT_VERSION=$(cd ../ && mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+KEYCLOAK_VERSION=$(cd ../ && mvn help:evaluate -Dexpression=version.org.keycloak -q -DforceStdout)
 
 echo "Project version: ${PROJECT_VERSION}"
 
@@ -15,6 +16,7 @@ fi
 
 echo "Kogito Image version: ${KOGITO_VERSION}"
 echo "KOGITO_VERSION=${KOGITO_VERSION}" > ".env"
+echo "KEYCLOAK_VERSION=${KEYCLOAK_VERSION}" >> ".env"
 
 if [ "$(uname)" == "Darwin" ]; then
    echo "Docker-compose don't support host-mode in MAC OS, that is used by jobs-service in this example"
