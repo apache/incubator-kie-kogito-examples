7c5a6169fc053ce906514802a23e1fba15ebb686
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2022-04-29 15:42:45 +1000

    KOGITO-7064 - Adjust models to implement Serializable

--- kogito-quarkus-examples/serverless-workflow-qas-service-showcase/query-answer-service/src/main/java/org/acme/PostgreSqlQueryRecordRepository.java
+++ kogito-quarkus-examples/serverless-workflow-qas-service-showcase/query-answer-service/src/main/java/org/acme/PostgreSqlQueryRecordRepository.java
@@ -16,34 +16,33 @@
 
 package org.acme;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.quarkus.arc.properties.IfBuildProperty;
 import io.quarkus.runtime.Startup;
-import io.vertx.mutiny.sqlclient.Row;
-import io.vertx.mutiny.sqlclient.RowIterator;
-import io.vertx.mutiny.sqlclient.RowSet;
-import io.vertx.mutiny.sqlclient.Tuple;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * This PostgreSqlQueryRecordRepository implementation is used when the application is build with the persistence profile.
  */
 @ApplicationScoped
 @Startup
-@IfBuildProperty(name = "kogito.persistence.type", stringValue = "postgresql")
+@IfBuildProperty(name = "kogito.persistence.type", stringValue = "jdbc")
 public class PostgreSqlQueryRecordRepository implements QueryRecordRepository {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PostgreSqlQueryRecordRepository.class);
@@ -64,13 +63,13 @@ public class PostgreSqlQueryRecordRepository implements QueryRecordRepository {
             ")";
 
     @Inject
-    io.vertx.mutiny.pgclient.PgPool client;
+    DataSource dataSource;
 
     @PostConstruct
     public void initDB() {
-        try {
+        try (Connection connection = dataSource.getConnection(); PreparedStatement st = connection.prepareStatement(CREATE_QUERY_RECORD_TABLE)) {
             LOGGER.debug("Initializing {} table.", QUERY_RECORD_TABLE);
-            client.query(CREATE_QUERY_RECORD_TABLE).execute().await().indefinitely();
+            st.execute();
         } catch (Exception e) {
             throw new QueryAnswerServiceException("An error was produced during " + QUERY_RECORD_TABLE + " initialization: " + e.getMessage(), e);
         }
@@ -78,48 +77,70 @@ public class PostgreSqlQueryRecordRepository implements QueryRecordRepository {
 
     @Override
     public void saveOrUpdate(QueryRecord queryRecord) {
-        client.preparedQuery("INSERT INTO " + QUERY_RECORD_TABLE + " (" + QUERY_RECORD_COLUMNS + ") " +
-                " VALUES ($1, $2, $3, $4, $5, $6) " +
-                "ON CONFLICT (processinstanceid) DO " +
-                "UPDATE SET query = $2, created = $3, status = $4, answer = $5, lastmodified = $6 " +
-                "RETURNING " + QUERY_RECORD_COLUMNS)
-                .execute(Tuple.tuple(Stream.of(
-                        queryRecord.getProcessInstanceId(),
-                        queryRecord.getQuery(),
-                        queryRecord.getCreated().toLocalDateTime(),
-                        queryRecord.getStatus(),
-                        queryRecord.getAnswer(),
-                        queryRecord.getLastModified().toLocalDateTime()).collect(toList())))
-                .await().indefinitely().value();
+        try (Connection connection = dataSource.getConnection();
+                PreparedStatement st = connection.prepareStatement("INSERT INTO " + QUERY_RECORD_TABLE + " (" + QUERY_RECORD_COLUMNS + ") " +
+                        " VALUES (?, ?, ?, ?, ?, ?) " +
+                        "ON CONFLICT (processinstanceid) DO " +
+                        "UPDATE SET query = ?, created = ?, status = ?, answer = ?, lastmodified = ?")) {
+            st.setString(1, queryRecord.getProcessInstanceId());
+            st.setString(2, queryRecord.getQuery());
+            st.setObject(3, queryRecord.getCreated().toLocalDateTime());
+            st.setString(4, queryRecord.getStatus());
+            st.setString(5, queryRecord.getAnswer());
+            st.setObject(6, queryRecord.getLastModified().toLocalDateTime());
+            st.setString(7, queryRecord.getQuery());
+            st.setObject(8, queryRecord.getCreated().toLocalDateTime());
+            st.setString(9, queryRecord.getStatus());
+            st.setString(10, queryRecord.getAnswer());
+            st.setObject(11, queryRecord.getLastModified().toLocalDateTime());
+            st.executeUpdate();
+        } catch (Exception e) {
+            throw new QueryAnswerServiceException("An error was produced during " + QUERY_RECORD_TABLE + " update: " + e.getMessage(), e);
+        }
+
     }
 
     @Override
     public QueryRecord get(String id) {
-        return client.preparedQuery("SELECT " + QUERY_RECORD_COLUMNS + " FROM " + QUERY_RECORD_TABLE + " WHERE processinstanceid = $1")
-                .execute(Tuple.of(id))
-                .onItem().transform(RowSet::iterator)
-                .onItem().transform(iterator -> iterator.hasNext() ? from(iterator.next()) : null)
-                .await().indefinitely();
+        try (Connection connection = dataSource.getConnection();
+                PreparedStatement st = connection.prepareStatement("SELECT " + QUERY_RECORD_COLUMNS + " FROM " + QUERY_RECORD_TABLE + " WHERE processinstanceid = ?")) {
+            st.setString(1, id);
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    return from(rs);
+                } else {
+                    return null;
+                }
+            }
+        } catch (Exception e) {
+            throw new QueryAnswerServiceException("An error was produced during " + QUERY_RECORD_TABLE + " find: " + e.getMessage(), e);
+        }
     }
 
-    private static QueryRecord from(Row row) {
+    private static QueryRecord from(ResultSet row) throws SQLException {
         QueryRecord queryRecord = new QueryRecord();
         queryRecord.setProcessInstanceId(row.getString("processinstanceid"));
         queryRecord.setQuery(row.getString("query"));
-        queryRecord.setCreated(ZonedDateTime.of(row.getLocalDateTime("created"), ZoneId.systemDefault()));
+        queryRecord.setCreated(ZonedDateTime.of(row.getObject("created", LocalDateTime.class), ZoneId.systemDefault()));
         queryRecord.setStatus(row.getString("status"));
         queryRecord.setAnswer(row.getString("answer"));
-        queryRecord.setLastModified(ZonedDateTime.of(row.getLocalDateTime("lastmodified"), ZoneId.systemDefault()));
+        queryRecord.setLastModified(ZonedDateTime.of(row.getObject("lastmodified", LocalDateTime.class), ZoneId.systemDefault()));
         return queryRecord;
     }
 
     @Override
     public List<QueryRecord> getAll() {
-        RowIterator<Row> rows = client.query("SELECT " + QUERY_RECORD_COLUMNS + " FROM " + QUERY_RECORD_TABLE).execute()
-                .onItem().transform(RowSet::iterator)
-                .await().indefinitely();
-        return StreamSupport.stream(((Iterable<Row>) () -> rows).spliterator(), false)
-                .map(PostgreSqlQueryRecordRepository::from)
-                .collect(toList());
+        try (Connection connection = dataSource.getConnection();
+                PreparedStatement st = connection.prepareStatement("SELECT " + QUERY_RECORD_COLUMNS + " FROM " + QUERY_RECORD_TABLE)) {
+            List<QueryRecord> result = new ArrayList<>();
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    result.add(from(rs));
+                }
+            }
+            return result;
+        } catch (Exception e) {
+            throw new QueryAnswerServiceException("An error was produced during " + QUERY_RECORD_TABLE + " find all: " + e.getMessage(), e);
+        }
     }
 }
