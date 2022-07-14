9a0471903aa3e9c9b4422bd45643f018b740eb4f
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2022-07-14 22:37:03 +1000

    Review subscription Quarkus test (#1328)

---
+++ serverless-workflow-examples/serverless-workflow-newsletter-subscription/subscription-flow/src/test/java/org/acme/newsletter/subscription/flow/SinkMock.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.newsletter.subscription.flow;
+
+import java.util.Collections;
+import java.util.Map;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+
+// Mocking our Knative broker, the new.subscriber event should be broadcast to it
+public class SinkMock implements QuarkusTestResourceLifecycleManager {
+
+    private WireMockServer wireMockServer;
+
+    @Override
+    public Map<String, String> start() {
+        wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
+        wireMockServer.start();
+        wireMockServer.stubFor(post("/").willReturn(aResponse().withBody("ok").withStatus(200)));
+
+        return Collections.singletonMap("mp.messaging.outgoing.kogito_outgoing_stream.url", wireMockServer.baseUrl());
+    }
+
+    @Override
+    public void stop() {
+        if (wireMockServer != null) {
+            wireMockServer.stop();
+        }
+    }
+
+    @Override
+    public void inject(Object testInstance) {
+        ((SubscriptionFlowIT) testInstance).sink = wireMockServer;
+    }
+}
--- serverless-workflow-examples/serverless-workflow-newsletter-subscription/subscription-flow/src/test/java/org/acme/newsletter/subscription/flow/SubscriptionFlowIT.java
+++ serverless-workflow-examples/serverless-workflow-newsletter-subscription/subscription-flow/src/test/java/org/acme/newsletter/subscription/flow/SubscriptionFlowIT.java
@@ -23,8 +23,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.acme.newsletter.subscription.service.Subscription;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.kie.kogito.testcontainers.quarkus.PostgreSqlQuarkusTestResource;
 
@@ -40,12 +38,9 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static io.restassured.RestAssured.given;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.acme.newsletter.subscription.flow.SubscriptionConstants.EMAIL;
@@ -55,35 +50,21 @@ import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 @QuarkusIntegrationTest
 @QuarkusTestResource(SubscriptionServiceMock.class)
+@QuarkusTestResource(SinkMock.class)
 @QuarkusTestResource(SubscriptionFlowIT.ConditionalPostgreSqlQuarkusTestResource.class)
 public class SubscriptionFlowIT {
-
-    // Mocking our Knative broker, the new.subscriber event should be broadcast to it
-    private static WireMockServer sink;
 
     static {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 
+    WireMockServer sink;
+
     /**
-     * Starts the "sink" server, which is is the endpoint that will receive our produced events
+     * Starts the "sink" server, which is the endpoint that will receive our produced events
      */
-    @BeforeAll
-    public static void startSink() {
-        sink = new WireMockServer(options().port(8181));
-        sink.start();
-        sink.stubFor(post("/").willReturn(aResponse().withBody("ok").withStatus(200)));
-    }
-
-    @AfterAll
-    public static void stopSink() {
-        if (sink != null) {
-            sink.stop();
-        }
-    }
-
     @Test
-    void verifySubscription() throws JsonProcessingException, InterruptedException {
+    void verifySubscription() throws JsonProcessingException {
         final ObjectMapper mapper = new ObjectMapper();
         final TypeReference<HashMap<String, Object>> typeRef = new TypeReference<>() {
         };
