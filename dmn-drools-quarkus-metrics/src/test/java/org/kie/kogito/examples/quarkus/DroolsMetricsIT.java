54e191548bcb4b32f51556d44bd39984ba368d78
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2023-03-01 22:48:26 +1000

    [1.13.x] Bump Quarkus version to 2.13.4.Final (#1469) (#1581)
    * [1.13.x] Bump Quarkus version to 2.13.4.Final (#1469)
    
    * [1.13.x] Bump Quarkus version to 2.13.4.Final
    
    * Setup Maven 3.8.6
    
    * Use QuarkusTestProperty
    
    Co-authored-by: Jenkins CI <noreply@redhat.com>
    Co-authored-by: radtriste <tradisso@redhat.com>
    Co-authored-by: Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
    
    * Review tests
    
    * Use fixed value for kogito.bom.version
    
    * Revert "Use fixed value for kogito.bom.version"
    
    This reverts commit dbe8baf2f26fcbfdabd4785e03bc591aefd4e07b.
    
    * Deps info
    
    * Revert "Deps info"
    
    This reverts commit 2748f82ef346f8d7e447b90c843c4b2cfeedaeea.
    
    * Update SB docker image
    
    * Use JIB to generate docker image
    
    * Await for dashboards lookup
    
    * Revert "Await for dashboards lookup"
    
    This reverts commit 8eab02e1117870211025adb9ee4582bc20114fee.
    
    * Config debug
    
    * Resource filters
    
    * Review resource filters
    
    * Review fixes
    
    * Use JIB to build Docker image
    
    * Revert "Use JIB to build Docker image"
    
    This reverts commit 88de703d7bdd0dcb4381f36a1988e134babb0faf.
    
    * Disable GHA cache
    
    * Revert "Disable GHA cache"
    
    This reverts commit b884320567655b57b881baa5fe91b3742d2e718e.
    
    * Test dep version
    
    * Revert "Test dep version"
    
    This reverts commit 806b8b8deb2c34cde3b46dc045eed0a05a1ddad6.
    
    * Revert "Config debug"
    
    This reverts commit 538480e2ca6596a8187bc66718d11021b7b30734.
    
    ---------
    
    Co-authored-by: kie-ci <kie-ci0@redhat.com>
    Co-authored-by: Jenkins CI <noreply@redhat.com>
    Co-authored-by: radtriste <tradisso@redhat.com>

--- dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/quarkus/DroolsMetricsIT.java
+++ dmn-drools-quarkus-metrics/src/test/java/org/kie/kogito/examples/quarkus/DroolsMetricsIT.java
@@ -17,20 +17,20 @@ package org.kie.kogito.examples.quarkus;
 
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
-@QuarkusTest
-public class DroolsMetricsTest {
+@QuarkusIntegrationTest
+public class DroolsMetricsIT {
 
     private static final String PROJECT_VERSION = ProjectMetadataProvider.getProjectVersion();
     private static final String PROJECT_ARTIFACT_ID = ProjectMetadataProvider.getProjectArtifactId();
 
     @Test
-    public void testDrlMetrics() throws Exception {
+    public void testDrlMetrics() {
         given()
                 .when()
                 .get("/metrics")
