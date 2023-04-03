d9601094dc19eb35d0455406badf522984cd1174
Tristan Radisson <tristan.radisson@gmail.com>
2023-04-03 11:11:27 +0200

    Apply suggestions from code review

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -50,7 +50,7 @@ Map getMultijobPRConfig(JenkinsFolder jobFolder) {
 
     // For Quarkus 3, run only runtimes PR check... for now
     if (EnvUtils.hasEnvironmentId(this, jobFolder.getEnvironmentName(), 'quarkus3')) {
-        jobConfig.jobs.retainAll { it.id == 'kogito-runtimes' }
+        jobConfig.jobs.retainAll { it.id == 'kogito-examples' }
     }
 
     return jobConfig
