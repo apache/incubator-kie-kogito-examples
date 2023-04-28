5dac543e3ec7bdc8a2c9333f664f8beb0f353c79
Tristan Radisson <tradisso@redhat.com>
2023-04-28 09:24:04 +0200

    Update jobs.groovy

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -19,7 +19,7 @@ import org.kie.jenkins.jobdsl.Utils
 jenkins_path = '.ci/jenkins'
 
 Map getMultijobPRConfig(JenkinsFolder jobFolder) {
-    String defaultBuildMvnOptsCurrent = jobFolder.getDefaultEnvVars().find { key, value -> "${key}" == "BUILD_MVN_OPTS_CURRENT" }?.value ?: ''
+    String defaultBuildMvnOptsCurrent = jobFolder.getDefaultEnvVarValue('BUILD_MVN_OPTS_CURRENT') ?: ''
     def jobConfig = [
         parallel: true,
         buildchain: true,
