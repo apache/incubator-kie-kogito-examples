2a28c6ae545622c95dfa3ca31b9df525b4c56e87
Walter Medvedeo <wmedvedeo@gmail.com>
2023-05-17 14:53:09 +0200

    KOGITO-9119: Example to showcase the embedded use case with timers (#1665)
    * KOGITO-9119: Example to showcase the embedded use case with timers
    
    * KOGITO-9119: Example to showcase the embedded use case with timers
        - Code review suggestions

--- serverless-workflow-examples/serverless-workflow-timeouts-showcase/scripts/delete-showcase-database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# commands sequence to remove the timeouts-showcase-database:
-
-kubectl delete deployment timeouts-showcase-database
-
-kubectl delete service timeouts-showcase-database
-
-kubectl delete secret timeouts-showcase-database
