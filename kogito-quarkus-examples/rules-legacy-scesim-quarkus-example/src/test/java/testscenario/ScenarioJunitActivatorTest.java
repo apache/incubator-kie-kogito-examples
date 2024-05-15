5764a1e4d7bbca197cf595fa16fa0b4a605ffe99
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2024-05-15 12:03:14 +0200

    [incubator-kie-issues#1209] Implemented scesim rules quarkus example

---
+++ kogito-quarkus-examples/rules-legacy-scesim-quarkus-example/src/test/java/testscenario/ScenarioJunitActivatorTest.java
@@ -0,0 +1,8 @@
+package testscenario;
+
+/**
+ * Do not remove this file
+ */
+@org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
+public class ScenarioJunitActivatorTest {
+}
