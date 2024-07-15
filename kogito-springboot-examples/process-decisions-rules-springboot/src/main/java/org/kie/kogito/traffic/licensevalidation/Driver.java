e79f1b5559da8843ac51cade338955d1286523cb
Jozef Marko <Jozef.Marko@ibm.com>
2024-07-15 17:55:48 +0200

    NO-ISSUE: Fix process-decisions-quarkus not starting due to missing getters/setters (#1981)

--- kogito-springboot-examples/process-decisions-rules-springboot/src/main/java/org/kie/kogito/traffic/licensevalidation/Driver.java
+++ kogito-springboot-examples/process-decisions-rules-springboot/src/main/java/org/kie/kogito/traffic/licensevalidation/Driver.java
@@ -61,6 +61,14 @@ public class Driver {
         this.licenseExpiration = licenseExpiration;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
