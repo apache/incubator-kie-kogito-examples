a2c841f50da62eff5fb3bc129f5a6318c6b5c504
Yeser Amer <yeser.amer@ibm.com>
2025-05-13 11:01:00 +0200

    WIP

--- kogito-quarkus-examples/dmn-resource-jar-quarkus-example/dmn-quarkus-consumer-example/src/test/java/testscenario/TestScenarioJunitActivatorTest.java
+++ kogito-quarkus-examples/dmn-resource-jar-quarkus-example/dmn-quarkus-consumer-example/src/test/java/testscenario/TestScenarioJunitActivatorTest.java
@@ -18,12 +18,14 @@
  */
 package testscenario;
 
+import org.drools.scenariosimulation.backend.runner.TestScenarioActivator;
+
 /**
- * KogitoJunitActivator is a custom JUnit runner that enables the execution of Test Scenario files (*.scesim).
+ * TestScenarioJunitActivator is a custom JUnit runner that enables the execution of Test Scenario files (*.scesim).
  * This activator class, when executed, will load all scesim files available in the project and run them.
  * Each row of the scenario will generate a test JUnit result.
  */
-@org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
-public class KogitoScenarioJunitActivatorTest {
+@TestScenarioActivator
+public class TestScenarioJunitActivatorTest {
 
 }
--- kogito-springboot-examples/rules-legacy-scesim-springboot-example/src/test/java/testscenario/TestScenarioJunitActivatorTest.java
+++ kogito-springboot-examples/rules-legacy-scesim-springboot-example/src/test/java/testscenario/TestScenarioJunitActivatorTest.java
@@ -18,9 +18,14 @@
  */
 package testscenario;
 
+import org.drools.scenariosimulation.backend.runner.TestScenarioActivator;
+
 /**
- * Do not remove this file
+ * TestScenarioJunitActivator is a custom JUnit runner that enables the execution of Test Scenario files (*.scesim).
+ * This activator class, when executed, will load all scesim files available in the project and run them.
+ * Each row of the scenario will generate a test JUnit result.
  */
-@org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
-public class ScenarioJunitActivatorTest {
+@TestScenarioActivator
+public class TestScenarioJunitActivatorTest {
+
 }
