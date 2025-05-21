7a3071ed8c203531da680f136a1701172216272b
Yeser Amer <yeser.amer@ibm.com>
2025-05-21 09:21:51 +0200

    WIP

--- kogito-quarkus-examples/rules-legacy-scesim-quarkus-example/src/test/java/testscenario/TestScenarioJunitActivatorTest.java
+++ kogito-quarkus-examples/rules-legacy-scesim-quarkus-example/src/test/java/testscenario/TestScenarioJunitActivatorTest.java
@@ -20,14 +20,11 @@ package testscenario;
 
 import org.drools.scenariosimulation.backend.runner.TestScenarioActivator;
 
-import io.quarkus.test.junit.QuarkusTest;
-
 /**
  * TestScenarioJunitActivator is a custom JUnit runner that enables the execution of Test Scenario files (*.scesim).
  * This activator class, when executed, will load all scesim files available in the project and run them.
  * Each row of the scenario will generate a test JUnit result.
  */
-@QuarkusTest
 @TestScenarioActivator
 public class TestScenarioJunitActivatorTest {
 
